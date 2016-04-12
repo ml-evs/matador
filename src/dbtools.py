8cb58648e669282144389e57c7eedb42d9aec1c2
Matthew Evans <me388@cam.ac.uk>
2016-04-12 18:01:28 +0100

    Allowed dbtools to drop indexes

---
+++ src/dbtools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+from __future__ import print_function
+import pymongo as pm
+import argparse
+import bson.json_util as json
+
+def dbstats():
+
+    client = pm.MongoClient()
+    db = client.crystals
+    repo = db.repo
+    print(json.dumps(db.command('dbstats'),indent=2))
+    for name in db.collection_names():
+        print(json.dumps(db.command('collstats', name),indent=2))
+    
+if __name__ == '__main__':
+    
+    client = pm.MongoClient()
+    db = client.crystals
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--drop', type=str,
+            help='drop named collection')
+    args = parser.parse_args() 
+    if args.drop != None:
+        print(args.drop)
+        db[str(args.drop)].dropIndexes()
+        db[str(args.drop)].drop()
+        print('dropped', str(args.drop))
+    dbstats()
+
