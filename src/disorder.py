c0339c792a527a801c9927db545cb8025b52d1ad
Matthew Evans <me388@cam.ac.uk>
2016-05-27 17:42:46 +0100

    Split disorder and hull

---
+++ src/disorder.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def disorder_hull(doc):
+    """ Broaden structures on phase diagram by 
+    a measure of local stoichiometry.
+    """
+    num_atoms = doc['num_atoms']
+    lat_cart  = doc['lattice_cart']
+    disps = np.zeros((num_atoms, num_atoms-1))
+    atoms = np.empty((num_atoms, num_atoms-1), dtype=str)
+    for i in range(num_atoms):
+        jindex = 0
+        for j in range(num_atoms):
+            temp_disp = np.zeros((3))
+            real_disp = np.zeros((3))
+            if i != j:
+                atoms[i, jindex] = doc['atom_types'][j]
+                for k in range(3):
+                    temp_disp[k] = (doc['positions_frac'][j][k] - doc['positions_frac'][i][k])
+                    if temp_disp[k] > 0.5:
+                        temp_disp[k] -= 1
+                    elif temp_disp[k] < -0.5:
+                        temp_disp[k] += 1
+                for k in range(3):
+                    for q in range(3):
+                        real_disp[q] += temp_disp[k]*lat_cart[k][q]
+                for k in range(3):
+                    disps[i,jindex] += real_disp[k]**2
+                jindex += 1
+    disps = np.sqrt(disps)
+    
+    def warren_cowley(atoms, disps):
+        nn_atoms = []
+        for i in range(len(atoms)):
+            nn_atoms.append(atoms[i][np.where(disps[i] < 3)])
+        count = np.zeros((2), dtype=float)
+        for i in range(len(nn_atoms)):
+            same_elem = doc['atom_types'][i][0]
+            for j in range(len(nn_atoms[i])):
+                if nn_atoms[i][j] == same_elem:
+                    count[0] += 1.0
+                    count[1] += 1.0
+                else:
+                    count[0] -= 1.0
+                    count[1] += 1.0
+        return count[0] / (4*(count[1]))
+    
+    def bond_disorder(atoms, disps):
+        nn_atoms = []
+        for i in range(len(atoms)):
+            nn_atoms.append(atoms[i][np.where(disps[i] < 3)])
+        count = np.zeros((2), dtype=float)
+        for i in range(len(nn_atoms)):
+            same_elem = doc['atom_types'][i][0]
+            for j in range(len(nn_atoms[i])):
+                if nn_atoms[i][j] == same_elem:
+                    count[0] += 1.0
+                else:
+                    count[1] += 1.0
+        
+        return count[0] / (4*(count[1]+count[0]))
+    
+    warren = False
+    if warren:
+        return warren_cowley(atoms, disps), warren
+    else:
+        return bond_disorder(atoms, disps), warren
--- src/hull.py
+++ src/hull.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python
 # coding: utf-8
 from __future__ import print_function
-# import related crysdb functionality
-# from fryan import DBQuery
-# import external libraries
 from scipy.spatial import ConvexHull
 from mpldatacursor import datacursor
 from bson.son import SON
 import pymongo as pm
 import matplotlib.pyplot as plt
+import re
+import numpy as np
+
 try:
     plt.style.use('bmh')
 except:
     pass
-import re
-import numpy as np
 
 class FryanConvexHull():
     """
@@ -171,69 +169,3 @@ class FryanConvexHull():
         ax.set_yticks([])
         # ax.set_ylim(0,np.max(voltages[1:])*1.5)
         plt.show()
-
-def disorder_hull(doc):
-    """ Broaden structures on phase diagram by 
-    a measure of local stoichiometry.
-    """
-    num_atoms = doc['num_atoms']
-    lat_cart  = doc['lattice_cart']
-    disps = np.zeros((num_atoms, num_atoms-1))
-    atoms = np.empty((num_atoms, num_atoms-1), dtype=str)
-    for i in range(num_atoms):
-        jindex = 0
-        for j in range(num_atoms):
-            temp_disp = np.zeros((3))
-            real_disp = np.zeros((3))
-            if i != j:
-                atoms[i, jindex] = doc['atom_types'][j]
-                for k in range(3):
-                    temp_disp[k] = (doc['positions_frac'][j][k] - doc['positions_frac'][i][k])
-                    if temp_disp[k] > 0.5:
-                        temp_disp[k] -= 1
-                    elif temp_disp[k] < -0.5:
-                        temp_disp[k] += 1
-                for k in range(3):
-                    for q in range(3):
-                        real_disp[q] += temp_disp[k]*lat_cart[k][q]
-                for k in range(3):
-                    disps[i,jindex] += real_disp[k]**2
-                jindex += 1
-    disps = np.sqrt(disps)
-    
-    def warren_cowley(atoms, disps):
-        nn_atoms = []
-        for i in range(len(atoms)):
-            nn_atoms.append(atoms[i][np.where(disps[i] < 3)])
-        count = np.zeros((2), dtype=float)
-        for i in range(len(nn_atoms)):
-            same_elem = doc['atom_types'][i][0]
-            for j in range(len(nn_atoms[i])):
-                if nn_atoms[i][j] == same_elem:
-                    count[0] += 1.0
-                    count[1] += 1.0
-                else:
-                    count[0] -= 1.0
-                    count[1] += 1.0
-        return count[0] / (4*(count[1]))
-    
-    def bond_disorder(atoms, disps):
-        nn_atoms = []
-        for i in range(len(atoms)):
-            nn_atoms.append(atoms[i][np.where(disps[i] < 3)])
-        count = np.zeros((2), dtype=float)
-        for i in range(len(nn_atoms)):
-            same_elem = doc['atom_types'][i][0]
-            for j in range(len(nn_atoms[i])):
-                if nn_atoms[i][j] == same_elem:
-                    count[0] += 1.0
-                else:
-                    count[1] += 1.0
-        
-        return count[0] / (4*(count[1]+count[0]))
-    
-    warren = False
-    if warren:
-        return warren_cowley(atoms, disps), warren
-    else:
-        return bond_disorder(atoms, disps), warren
