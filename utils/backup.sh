eaa8e448ea1a283287ec090cb85f42e4afe73ccc
Matthew Evans <me388@cam.ac.uk>
2016-06-13 22:09:59 +0100

    Actually na

--- utils/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tar -cjvf ~/ajm_repo.tar.bz2 /u/fs1/morris/structure_repository
-ssh me388@henri.tcm.phy.private.cam.ac.uk 'mv /BIG/me388/ajm_repo.tar.bz2 /BIG/me388/ajm_repo.tar.bz2_bak'
-scp ~/ajm_repo.tar.bz2 me388@henri.tcm.phy.private.cam.ac.uk:/BIG/me388
-rm ~/ajm_repo.tar.bz2
-echo "Backup created successfully."
-
-
