7b919efedf22c4cf6dc18ca2944ca31433a124cc
Matthew Evans <me388@cam.ac.uk>
2016-06-18 14:39:15 +0100

    Tidied up dbtools

--- src/dbtools.py
+++ src/dbtools.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
+""" A file containing small db utlities, like
+dropping collections.
+"""
 from __future__ import print_function
 import pymongo as pm
 from os import uname
 import argparse
 import bson.json_util as json
 
-def dbstats():
 
+def dbstats():
     local = uname()[1]
     if local == 'cluster2':
         remote = 'node1'
@@ -14,27 +17,24 @@ def dbstats():
         remote = None
     client = pm.MongoClient(remote)
     db = client.crystals
-    repo = db.repo
-    print(json.dumps(db.command('dbstats'),indent=2))
+    print(json.dumps(db.command('dbstats'), indent=2))
     for name in db.collection_names():
-        print(json.dumps(db.command('collstats', name),indent=2))
-    
+        print(json.dumps(db.command('collstats', name), indent=2))
+
 if __name__ == '__main__':
-    
+
     client = pm.MongoClient()
     db = client.crystals
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--drop', type=str,
-            help='drop named collection')
-    args = parser.parse_args() 
-    if args.drop != None:
-        print(args.drop)
+                        help='drop named collection')
+    args = parser.parse_args()
+    if args.drop is not None:
         try:
             db[str(args.drop)].dropIndexes()
         except:
             pass
         db[str(args.drop)].drop()
-        print('dropped', str(args.drop))
+        print('Collection', str(args.drop), 'dropped.')
     dbstats()
-
