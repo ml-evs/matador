65a71aa13861aa91bd992d117c2c06c2cce7795b
me388 <me388@cam.ac.uk>
2016-07-03 17:48:32 +0100

    Made run3 more robust and fixed concurrency

--- src/run3.py
+++ src/run3.py
@@ -39,6 +39,8 @@ class BatchRun:
         self.args = kwargs
         self.debug = self.args.get('debug')
         self.all_cores = mp.cpu_count()
+        self.seed = self.args.get('seed')
+        valid = True
         if self.args.get('nprocesses') is not None:
             self.nprocesses = self.args['nprocesses']
         else:
@@ -58,52 +60,20 @@ class BatchRun:
                 if root == '.':
                     if file.endswith('.res'):
                         self.file_lists['res'].append(file)
-                    elif file.endswith('.cell'):
-                        self.file_lists['cell'].append(file)
-                    elif file.endswith('.param'):
-                        self.file_lists['param'].append(file)
-        valid = True
-        if self.args.get('seed') is not None:
-            if self.args.get('seed') + '.cell' in self.file_lists['cell']:
-                cell_seed = self.args.get('seed')
-            else:
-                print(param_seed + '.cell not found!')
-                valid = False
-            if self.args.get('seed') + '.param' in self.file_lists['param']:
-                param_seed = self.args.get('seed')
-            else:
-                print(param_seed + '.param not found!')
-                valid = False
-        else:
-            # check for correct multiplicity of file types
-            if len(self.file_lists['cell']) != 1:
-                valid = False
-                print('run3 requires exactly 1 cell file in folder, found',
-                      len(self.file_lists['cell']))
-            if len(self.file_lists['param']) != 1:
-                valid = False
-                print('run3 requires exactly 1 param file in folder, found',
-                      len(self.file_lists['param']))
-            # read cell and param files into dicts
-            cell_seed = self.file_lists['cell'][0]
-            param_seed = self.file_lists['param'][0]
         if len(self.file_lists['res']) < 1:
             valid = False
             print('run3 requires at least 1 res file in folder, found',
                   len(self.file_lists['res']))
+        self.cell_dict, cell_success = cell2dict(self.seed + '.cell')
+        if not cell_success:
+            valid = False
+            print('Failed to parse cell file')
+        self.param_dict, param_success = param2dict(self.seed + '.param', db=False)
+        if not param_success:
+            valid = False
+            print('Failed to parse cell file')
         if not valid:
             exit('Exiting...')
-        self.cell_dict, cell_success = cell2dict(cell_seed)
-        if not cell_success:
-            print('Failed to parse cell file')
-        self.param_dict, param_success = param2dict(param_seed, db=False)
-        if not param_success:
-            print('Failed to parse cell file')
-        success = False
-        if cell_success and param_success:
-            success = True
-        if not success:
-            exit()
         # delete source from cell and param
         del self.cell_dict['source']
         del self.param_dict['source']
@@ -119,7 +89,6 @@ class BatchRun:
         if not isfile(self.paths['completed_fname']):
             with open(self.paths['completed_fname'], 'a'):
                 pass
-        pass
 
     def spawn(self):
         # begin running calculations that have not already started
@@ -131,7 +100,7 @@ class BatchRun:
         try:
             for proc in procs:
                 proc.start()
-        except(KeyboardInterrupt, SystemExit):
+        except(KeyboardInterrupt, SystemExit, RuntimeError):
             for proc in procs:
                 proc.terminate()
             exit('Killing running jobs and exiting...')
@@ -141,7 +110,7 @@ class BatchRun:
         failed or finished to completion. """
         for res in res_list:
             running = False
-            with open(paths['jobs_fname'], 'r') as job_file:
+            with open(paths['jobs_fname'], 'rw') as job_file:
                 flines = job_file.readlines()
                 for line in flines:
                     if res in line:
@@ -159,12 +128,15 @@ class BatchRun:
                                 cell_dict=self.cell_dict,
                                 debug=self.debug)
                     print('Completed', res)
-                except(KeyboardInterrupt, SystemExit):
+                except(KeyboardInterrupt, SystemExit, RuntimeError):
                     raise SystemExit
         return
 
 class FullRelaxer:
-    """ Turn res file name into "full relax" CASTEP job.
+    """ Perform full relxation of res input by first doing 
+    4 rough optimisations with only a few iterations, followed by
+    4 larger optimisations with many iterations,
+    e.g. 4 lots of 2 then 4 lots of geom_max_iter/4.
     """
     def __init__(self, paths, ncores, res, param_dict, cell_dict, debug):
         """ Make the files to run the calculation and handle
@@ -195,7 +167,12 @@ class FullRelaxer:
         then continue with the remainder of steps.
         """
         seed = self.seed
-        geom_max_iter_list = [2, 2, 2, 2, 4, calc_doc['geom_max_iter']]
+        geom_max_iter_list = [2, 2, 2, 2, 
+                              int(calc_doc['geom_max_iter']/4),
+                              int(calc_doc['geom_max_iter']/4),
+                              int(calc_doc['geom_max_iter']/4),
+                              int(calc_doc['geom_max_iter']/4)]
+        # relax structure
         for num_iter in geom_max_iter_list:
             calc_doc['geom_max_iter'] = num_iter
             try:
@@ -204,29 +181,12 @@ class FullRelaxer:
                     remove(seed+'.param')
                 if isfile(seed + '.cell'):
                     remove(seed+'.cell')
-            except:
-                print_exc()
-                print('Failed to clean up...')
-                raise RuntimeError
-            try:
                 # write new param and cell
                 doc2param(calc_doc, seed, hash_dupe=False)
                 doc2cell(calc_doc, seed, hash_dupe=False, copy_pspots=False)
-            except:
-                print('Failed to prepare calc')
-                system('rm ' + seed + '.cell')
-                system('rm ' + seed + '.param')
-                raise RuntimeError
-            try:
                 # run CASTEP
                 process = self.castep()
                 process.communicate()
-            except:
-                print_exc()
-                system('rm ' + seed + '.cell')
-                system('rm ' + seed + '.param')
-                raise SystemExit('Killing jobs...')
-            try:
                 # scrape new structure from castep file
                 opti_dict, success = castep2dict(seed + '.castep', db=False)
                 if not success and opti_dict == '':
@@ -241,8 +201,9 @@ class FullRelaxer:
                     system('mv ' + seed + '.castep' + ' completed/' + seed + '.castep')
                     system('mv ' + seed + '.param' + ' completed/' + seed + '.param')
                     system('mv ' + seed + '.cell' + ' completed/' + seed + '.cell')
+                    system('mv ' + seed + '-out.cell' + ' completed/' + seed + '-out.cell')
                     # clean up rest of files
-                    system('rm ' + seed + '.*')
+                    self.tidy_up()
                     return True
                 else:
                     err_file = seed + '*.err'
@@ -251,19 +212,23 @@ class FullRelaxer:
                             self.mv_to_bad()
                             return False
                     calc_doc.update(opti_dict)
-                    if isfile(seed + '-save.res'):
-                        system('rm ' + seed + '-save.res')
-                    doc2res(calc_doc, seed + '-save')
             except:
                 print_exc()
-                raise RuntimeError('Failed to restart calculation...')
+                self.mv_to_bad()
+                self.tidy_up()
+                return False
 
     def mv_to_bad(self):
         """ Move all associated files to bad_castep. """
         if not exists('bad_castep'):
             makedirs('bad_castep')
-        print('CASTEP crashed... skipping...')
+        print('Something went wrong, moving files to bad_castep')
         system('mv ' + self.seed + '* bad_castep')
+        return
+
+    def tidy_up(self):
+        """ Delete all run3 created files before quitting. """
+        system('rm ' + seed + '*')
         return
 
     def castep(self):
@@ -287,14 +252,14 @@ if __name__ == '__main__':
                      value of geom_max_iter in the param file.',
         epilog='Written by Matthew Evans (2016), based primarily on run.pl and run2.pl \
                 by Chris Pickard and Andrew Morris and PyAIRSS CastepRunner by Jamie Wynn.')
+    parser.add_argument('seed', type=str,
+                        help='cell and param seed to use as template for calculations')
     parser.add_argument('-nc', '--ncores', type=int,
                         help='number of cores CASTEP per job [DEFAULT=cpu_count/nprocesses]')
     parser.add_argument('-np', '--nprocesses', type=int,
                         help='number of concurrent calculations [DEFAULT=1]')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='debug output')
-    parser.add_argument('-s', '--seed', 
-                        help='manually enter seed if more than one param or cell')
     args = parser.parse_args()
     runner = BatchRun(ncores=args.ncores, nprocesses=args.nprocesses, debug=args.debug, seed=args.seed)
     try:
