4ee5f018aabfd6f0a2bf738d5a400b68b74a5c8c
me388 <me388@cam.ac.uk>
2016-07-06 18:06:44 +0100

    Added convergence functionality to run3

--- src/run3.py
+++ src/run3.py
@@ -18,6 +18,7 @@ from os.path import isfile, exists
 from collections import defaultdict
 from scrapers.castep_scrapers import cell2dict, param2dict
 from scrapers.castep_scrapers import res2dict, castep2dict
+from print_utils import print_failure, print_success, print_warning
 from export import doc2cell, doc2param, doc2res
 from traceback import print_exc
 import bson.json_util as json
@@ -40,6 +41,7 @@ class BatchRun:
         self.debug = self.args.get('debug')
         self.all_cores = mp.cpu_count()
         self.seed = self.args.get('seed')
+        self.conv_cutoff = self.args.get('conv_cutoff')
         valid = True
         if self.args.get('nprocesses') is not None:
             self.nprocesses = self.args['nprocesses']
@@ -50,7 +52,7 @@ class BatchRun:
         else:
             self.ncores = self.all_cores / self.nprocesses
         if self.ncores*self.nprocesses > self.all_cores:
-            print('Requested more cores (' + str(self.ncores*self.nprocesses) +
+            print_failure('Requested more cores (' + str(self.ncores*self.nprocesses) +
                   ') than available (' + str(self.all_cores) + ').')
             exit('Exiting...')
         # scan directory for files to run
@@ -62,16 +64,19 @@ class BatchRun:
                         self.file_lists['res'].append(file)
         if len(self.file_lists['res']) < 1:
             valid = False
-            print('run3 requires at least 1 res file in folder, found',
-                  len(self.file_lists['res']))
+            print_failure('run3 requires at least 1 res file in folder, found' + 
+                          str(len(self.file_lists['res'])))
         self.cell_dict, cell_success = cell2dict(self.seed + '.cell', db=False)
         if not cell_success:
             valid = False
-            print('Failed to parse cell file')
+            print_failure('Failed to parse cell file')
         self.param_dict, param_success = param2dict(self.seed + '.param', db=False)
         if not param_success:
             valid = False
-            print('Failed to parse cell file')
+            print_failure('Failed to parse cell file')
+        if int(self.param_dict['geom_max_iter']) < 20:
+            valid = False
+            print_failure('geom_max_iter is only ' + str(self.param_dict['geom_max_iter']) + '... quitting.')
         if not valid:
             exit('Exiting...')
         # delete source from cell and param
@@ -126,7 +131,8 @@ class BatchRun:
                                 res=res,
                                 param_dict=self.param_dict,
                                 cell_dict=self.cell_dict,
-                                debug=self.debug)
+                                debug=self.debug,
+                                conv_cutoff=self.conv_cutoff)
                     print('Completed', res)
                 except(KeyboardInterrupt, SystemExit, RuntimeError):
                     raise SystemExit
@@ -138,7 +144,7 @@ class FullRelaxer:
     4 larger optimisations with many iterations,
     e.g. 4 lots of 2 then 4 lots of geom_max_iter/4.
     """
-    def __init__(self, paths, ncores, res, param_dict, cell_dict, debug):
+    def __init__(self, paths, ncores, res, param_dict, cell_dict, debug, conv_cutoff):
         """ Make the files to run the calculation and handle
         the calling of CASTEP itself.
         """
@@ -146,6 +152,9 @@ class FullRelaxer:
         self.ncores = ncores
         self.executable = 'castep'
         self.debug = debug
+        self.conv_cutoff_bool = True if conv_cutoff is not None else False
+        if self.conv_cutoff_bool:
+            self.conv_cutoff = conv_cutoff
         # read in initial structure
         res_dict, success = res2dict(res, db=False)
         calc_doc = res_dict
@@ -158,22 +167,33 @@ class FullRelaxer:
         calc_doc['task'] = 'geometryoptimization'
         if self.debug:
             print(json.dumps(calc_doc, indent=2))
-        self.success = self.relax(calc_doc)
+        if self.conv_cutoff_bool:
+            for cutoff in self.conv_cutoff:
+                calc_doc.update({'cut_off_energy': cutoff})
+                seed = self.seed + '_' + str(cutoff) + 'eV'
+                self.success = self.relax(calc_doc, seed)
+        else:
+            self.success = self.relax(calc_doc, self.seed)
         if not success:
             self.mv_to_bad()
 
-    def relax(self, calc_doc):
+    def relax(self, calc_doc, seed):
         """ Set up the calculation to perform 4 sets of two steps,
         then continue with the remainder of steps.
         """
-        seed = self.seed
-        geom_max_iter_list = [2, 2, 2, 2, calc_doc['geom_max_iter']]
+        num_rough_iter = 4
+        num_fine_iter = 4
+        rough_iter = 2
+        fine_iter = int(calc_doc['geom_max_iter'])/num_fine_iter
+        geom_max_iter_list = (num_rough_iter * [rough_iter])
+        geom_max_iter_list.extend(num_fine_iter * [fine_iter])
+        print(geom_max_iter_list)
         # relax structure
         for ind, num_iter in enumerate(geom_max_iter_list):
             calc_doc['geom_max_iter'] = num_iter
             try:
                 # delete any existing files
-                if isfile(self.seed + '.param'):
+                if isfile(seed + '.param'):
                     remove(seed+'.param')
                 if isfile(seed + '.cell'):
                     remove(seed+'.cell')
@@ -181,17 +201,17 @@ class FullRelaxer:
                 doc2param(calc_doc, seed, hash_dupe=False)
                 doc2cell(calc_doc, seed, hash_dupe=False, copy_pspots=False)
                 # run CASTEP
-                process = self.castep()
+                process = self.castep(seed)
                 process.communicate()
                 # scrape new structure from castep file
                 opti_dict, success = castep2dict(seed + '.castep', db=False)
                 if not success and opti_dict == '':
-                    print('Failed to scrape castep file...')
+                    print_warning('Failed to scrape castep file...')
                     return False
                 if opti_dict['optimised'] == True:
                     if not exists('completed'):
                         makedirs('completed')
-                    print('Successfully relaxed', seed)
+                    print_success('Successfully relaxed ' + seed)
                     # write res and castep file out to completed folder
                     doc2res(opti_dict, 'completed/' + seed)
                     system('mv ' + seed + '.castep' + ' completed/' + seed + '.castep')
@@ -199,50 +219,50 @@ class FullRelaxer:
                     system('mv ' + seed + '.cell' + ' completed/' + seed + '.cell')
                     system('mv ' + seed + '-out.cell' + ' completed/' + seed + '-out.cell')
                     # clean up rest of files
-                    self.tidy_up()
+                    self.tidy_up(seed)
                     return True
                 elif ind == len(geom_max_iter_list) - 1:
-                    print('Failed to optimise', self.seed)
-                    self.mv_to_bad()
+                    print_warning('Failed to optimise ' + seed)
+                    self.mv_to_bad(seed)
                 else:
                     err_file = seed + '*.err'
                     for globbed in glob.glob(err_file):
                         if isfile(globbed):
-                            self.mv_to_bad()
+                            self.mv_to_bad(seed)
                             return False
                     calc_doc.update(opti_dict)
             except(SystemExit, KeyboardInterrupt):
-                self.mv_to_bad()
-                self.tidy_up()
+                self.mv_to_bad(seed)
+                self.tidy_up(seed)
                 raise SystemExit
             except:
                 print_exc()
-                self.mv_to_bad()
-                self.tidy_up()
+                self.mv_to_bad(seed)
+                self.tidy_up(seed)
                 return False
 
-    def mv_to_bad(self):
+    def mv_to_bad(self, seed):
         """ Move all associated files to bad_castep. """
         if not exists('bad_castep'):
             makedirs('bad_castep')
         print('Something went wrong, moving files to bad_castep')
-        system('mv ' + self.seed + '* bad_castep')
+        system('mv ' + seed + '* bad_castep')
         return
 
-    def tidy_up(self):
+    def tidy_up(self, seed):
         """ Delete all run3 created files before quitting. """
-        system('rm ' + self.seed + '*')
+        system('rm ' + seed + '*')
         return
 
-    def castep(self):
+    def castep(self, seed):
         """ Calls CASTEP on desired seed with desired number of cores. 
         Errors piped to /dev/null for now...
         """
         if self.ncores == 1:
-            process = sp.Popen(['nice', '-n', '15', self.executable, self.seed])
+            process = sp.Popen(['nice', '-n', '15', self.executable, seed])
         else:
             process = sp.Popen(['nice', '-n', '15', 'mpirun', '-n', str(self.ncores),
-                                self.executable, self.seed])
+                                self.executable, seed])
         return process
 
 
@@ -263,8 +283,11 @@ if __name__ == '__main__':
                         help='number of concurrent calculations [DEFAULT=1]')
     parser.add_argument('-d', '--debug', action='store_true',
                         help='debug output')
+    parser.add_argument('--conv_cutoff', type=int, nargs='+',
+                        help='provide a list of cutoffs to run all structures at')
     args = parser.parse_args()
-    runner = BatchRun(ncores=args.ncores, nprocesses=args.nprocesses, debug=args.debug, seed=args.seed)
+    runner = BatchRun(ncores=args.ncores, nprocesses=args.nprocesses, debug=args.debug, seed=args.seed,
+                      conv_cutoff=args.conv_cutoff)
     try:
         runner.spawn()
     except(KeyboardInterrupt, SystemExit):
