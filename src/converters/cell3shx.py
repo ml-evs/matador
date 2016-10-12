071f60bfddca0906371dfb704921540863566c62
me388 <me388@cam.ac.uk>
2016-10-12 15:45:14 +0100

    Added new converters

---
+++ src/converters/cell3shx.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# coding: utf-8
+from scrapers import castep_scrapers
+from export import doc2res
+from print_utils import print_notify, print_success, print_failure
+from sys import argv
+
+fnames = argv[1:]
+for fname in fnames:
+    print_notify('Reading ' + fname)
+    cell_dict, success = castep_scrapers.cell2dict(fname, db=False)
+    if success:
+        doc2res(cell_dict, fname.replace('.cell', ''), copy_pspots=False, hash_dupe=True)
+        print_success('Wrote .res file to ' + fname.replace('.cell', '') + '.res')
+    else:
+        print_failure('Unable to find final structure in ' + fname + '.')
+print_success('Completed!')
