3683cb1964fd4feb6beb53e37314cb6d8f2128d0
Matthew Evans <me388@cam.ac.uk>
2016-10-19 15:14:06 +0100

    Improved spglib interface

--- src/refine.py
+++ src/refine.py
@@ -14,31 +14,45 @@ class Refiner:
     def __init__(self, cursor):
         wrong_count = dict()
         try:
-            total = len(cursor)
-        except:
-            total = cursor.count()
-        tested_count = 0
-        failed_count = 0
-        symprec = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
-        for sym in symprec:
-            wrong_count[sym] = 0
-        for doc in cursor:
             try:
-                tested_count += 1
-                cell = (doc['lattice_cart'],
-                        doc['positions_frac'],
-                        [get_atomic_number(elem) for elem in doc['atom_types']])
-                for sym in symprec:
-                    sg = spg.get_spacegroup(cell, symprec=sym)
-                    if sg.split()[0] != doc['space_group']:
-                        # print((sym, ':', sg + ' ' + doc['space_group']), end='\t')
-                        wrong_count[sym] += 1
-                        # print((sym, ':', sg + ' ' + doc['space_group']), end='\t')
-                # print('\n')
+                total = len(cursor)
             except:
-                failed_count += 1
-                pass
-        for sym in symprec:
-            print('Precision: ', sym, '# wrong:', wrong_count[sym])
-        print('# tested:', tested_count, 'total:', total)
-        print('# failed:', failed_count) 
+                total = cursor.count()
+            tested_count = 0
+            failed_count = 0
+            symprec = [1e-1, 1e-2, 1e-3, 1e-4, 1e-5]
+            print('Checking space groups...')
+            for sym in symprec:
+                wrong_count[sym] = 0
+            for ind, doc in enumerate(cursor):
+                try:
+                    tested_count += 1
+                    cell = (doc['lattice_cart'],
+                            doc['positions_frac'],
+                            [get_atomic_number(elem) for elem in doc['atom_types']])
+                    for sym in symprec:
+                        sg = spg.get_spacegroup(cell, symprec=sym)
+                        if sg.split()[0] != doc['space_group']:
+                            print((sym, ':', sg + ' ' + doc['space_group']), end='\t')
+                            wrong_count[sym] += 1
+                            # print((sym, ':', sg + ' ' + doc['space_group']), end='\t')
+                    print('\n')
+                except:
+                    failed_count += 1
+                    pass
+                print('# tested:', tested_count, '# total:', total, end='\r')
+            print('\n')
+            for sym in symprec:
+                print('Precision: ', sym, '\t# wrong:', wrong_count[sym])
+            print('\n')
+            print('# tested:', tested_count)
+            print('# failed:', failed_count) 
+            print('# total:', total)
+        except:
+            for sym in symprec:
+                print('Precision: ', sym, '# wrong:', wrong_count[sym])
+            print('# tested:', tested_count)
+        try:
+            cursor.close()
+        except:
+            pass
