65377e0d142690b6807241493aed5aebdd7f1c84
Matthew Evans <me388@cam.ac.uk>
2017-01-03 16:49:09 +0000

    Round latvec to 8dp

--- src/export.py
+++ src/export.py
@@ -405,7 +405,7 @@ def doc2res(doc, path, info=True, hash_dupe=True, *args):
             f.write('1.0 ')
             for vec in doc['lattice_abc']:
                 for coeff in vec:
-                    f.write(' ' + str(round(coeff, 5)))
+                    f.write(' ' + str(round(coeff, 8)))
             f.write('\n')
             f.write('LATT -1\n')
             f.write('SFAC \t')
