fccd1bc1d1757155255e84ac385a1655b939ff34
Matthew Evans <me388@cam.ac.uk>
2017-03-31 13:32:28 +0100

    Made plotting deps optional for testing purposes

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup, find_packages
 from subprocess import check_output
 try:
-    __version__ = check_output(["git", "describe", "--tags"]).decode("utf-8").strip()
-    __version__ += '-' + check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"]).decode("utf-8").strip()
+    __version__ = check_output(['git', 'describe', '--tags']).decode('utf-8').strip()
+    __version__ += '-' + (check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD'])
+                          .decode('utf-8').strip())
 except:
     __version__ = 'xxx'
 
@@ -21,20 +22,23 @@ setup(name='matador',
           'pymongo>=3.3.0',
           'periodictable>=1.4',
           'spglib>=1.9',
-          'matplotlib==1.5',
-          'bokeh>=0.12',
-          'python-ternary',
           'ascii_graph>=1.2'],
       scripts=['bin/matador', 'bin/run3', 'bin/oddjob'],
-      dependency_links=["https://github.com/marcharper/python-ternary/tarball/master"],
+      dependency_links=['https://github.com/marcharper/python-ternary/tarball/master'],
       package_data={'matador.scrapers': ['words', 'nouns'],
                     'matador.scrapers.tests': ['data/*'],
                     'matador.tests': ['data/*']},
-      test_suite="matador.tests",
+      test_suite='matador.tests',
       extras_require={
-          'pdffit': ["diffpy.Structure", "diffpy.srfit"],
-          'scrape_oqmd': ["mysqlclient"],
-          'docs': ["sphinx"],
-          'viz': ["ase", "nglview"],
+          'pdffit': ['diffpy.Structure',
+                     'diffpy.srfit'],
+          'scrape_oqmd': ['mysqlclient'],
+          'docs': ['sphinx'],
+          'viz': ['ase',
+                  'nglview'],
+          'plotting': ['matplotlib==1.5',
+                       'python-ternary',
+                       'bokeh>=0.12']
+
       },
       zip_safe=False)
