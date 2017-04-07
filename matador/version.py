5aecf919f9039bea8b959d5fcf338fabbde28fb0
Matthew Evans <me388@cam.ac.uk>
2017-04-07 21:01:49 +0100

    Removed redundant deps

--- matador/version.py
+++ matador/version.py
@@ -1,7 +1,3 @@
-from subprocess import check_output
-from matador.utils.print_utils import print_warning
-from os.path import realpath, dirname
-from os import getcwd, chdir
 from pkg_resources import require
 
 __version__ = require('matador')[0].version
