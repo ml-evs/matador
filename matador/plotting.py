7bc7a687fd75ba5d0c6fc3caeceb7d0dfbd5de11
Matthew Evans <me388@cam.ac.uk>
2017-09-27 14:30:27 +0100

    Compute bandstructure labels from structure in cell file, using seekpath

--- matador/plotting.py
+++ matador/plotting.py
@@ -1,9 +1,24 @@
-def plot_spectral(seed, bandstructure=True, dos=False, **kwargs):
+""" This file implements several useful plotting routines. """
+
+
+def plot_spectral(seed, bandstructure=True, dos=False, cell=False, **kwargs):
     """ Plot bandstructure and optional DOS from <seed>.bands and
     <seed>.adaptive.dat file.
+
+    Input:
+
+        | seed: str, filename of bands file
+
+    Args:
+
+        | bandstructure : bool, whether to plot bandstructure
+        | dos           : bool, whether to plot density of states
+        | cell          : bool, whether to work out correct labels from structure in cell file
+
     """
-    from matador.scrapers.castep_scrapers import bands2dict
-    from matador.utils.cell_utils import get_special_kpoints_for_lattice, get_crystal_system
+    from matador.scrapers.castep_scrapers import bands2dict, cell2dict
+    from matador.utils.cell_utils import doc2spg
+    from seekpath import get_path
     import numpy as np
     import matplotlib.pyplot as plt
     import seaborn as sns
@@ -12,6 +27,9 @@ def plot_spectral(seed, bandstructure=True, dos=False, **kwargs):
     colours = sns.color_palette()
     valence = colours[0]
     conduction = colours[1]
+    crossing = colours[2]
+    # conduction = colours[0]
+    # crossing = colours[0]
 
     if kwargs.get('plot_window') is not None:
         plot_window = (-kwargs.get('plot_window'), kwargs.get('plot_window'))
@@ -27,71 +45,104 @@ def plot_spectral(seed, bandstructure=True, dos=False, **kwargs):
                                                  'left': 0.15})
         ax_bs = ax_grid[0]
         ax_dos = ax_grid[1]
+    elif not bandstructure and dos:
+        fig, ax_dos = plt.subplots(figsize=(7, 5))
 
     seed = seed.replace('.bands', '')
     if bandstructure:
         bs, s = bands2dict(seed + '.bands')
-        path = np.linspace(0, 1, int(bs['num_kpoints']))
-        for ns in range(bs['num_spins']):
-            for nb in range(bs['num_bands']):
-                if np.mean(bs['eigenvalues_k_s'][ns][nb]) < 0:
-                    colour = valence
-                else:
-                    colour = conduction
-                ax_bs.plot(path, bs['eigenvalues_k_s'][ns][nb], c=colour, lw=1, marker=None)
+        path = np.linspace(0, 1, int(bs['num_kpoints'])-len(bs['kpoint_branches'])+1)
+        for branch_ind, branch in enumerate(bs['kpoint_branches']):
+            for ns in range(bs['num_spins']):
+                for nb in range(bs['num_bands']):
+                    if np.max(bs['eigenvalues_k_s'][ns][nb][branch]) < 0:
+                        colour = valence
+                    elif np.min(bs['eigenvalues_k_s'][ns][nb][branch]) > 0:
+                        colour = conduction
+                    elif np.min(bs['eigenvalues_k_s'][ns][nb][branch]) < 0 and np.max(bs['eigenvalues_k_s'][ns][nb][branch]) > 0:
+                        colour = crossing
+                    else:
+                        colour = 'black'
+                    ax_bs.plot(path[(np.asarray(branch)-branch_ind).tolist()], bs['eigenvalues_k_s'][ns][nb][branch], c=colour, lw=1, marker=None)
+            if branch_ind != len(bs['kpoint_branches'])-1:
+                ax_bs.axvline(path[branch[-1]-branch_ind], ls='-.', lw=1, c='grey')
         ax_bs.axhline(0, ls='--', lw=1, c='grey')
         ax_bs.set_ylim(plot_window)
         ax_bs.set_ylabel('$\epsilon_k$ (eV)')
         ax_bs.set_xlim(0, 1)
         ax_bs.yaxis.grid(False)
-        lattice = get_crystal_system(bs['lattice_cart'])
-        special_points = get_special_kpoints_for_lattice(lattice, bs['lattice_cart'])
         xticks = []
         xticklabels = []
         shear_planes = []
-        blacklist = []
-        for ind, kpt in enumerate(bs['kpoint_path']):
-            if ind != len(bs['kpoint_path'])-1:
-                next_point = bs['kpoint_path'][ind+1]
-            else:
-                next_point = bs['kpoint_path'][ind]
-            for label, point in special_points.items():
-                if np.allclose(point, kpt):
-                    if label in blacklist:
-                        continue
-                    if label == 'G':
-                        label = '\Gamma'
-                    if np.sqrt(np.sum((np.asarray(next_point) - np.asarray(point))**2)) > 2*bs['kpoint_path_spacing']:
-                        for new_label, new_point in special_points.items():
-                            if np.allclose(new_point, next_point):
-                                label = '{}|{}'.format(label, new_label)
-                                shear_planes.append(path[ind])
-                                blacklist = new_label
-                    label = '${}$'.format(label)
-                    xticklabels.append(label)
-                    xticks.append(path[ind])
-                    break
+        labelled = []
+
+        if cell:
+            doc, success = cell2dict(seed + '.cell', db=False, verbosity=10, outcell=True, positions=True)
+            if success:
+                spg_structure = doc2spg(doc)
+                if spg_structure is not False:
+                    seekpath_results = get_path(spg_structure)
+                path_labels = seekpath_results['point_coords']
+
+            for branch_ind, branch in enumerate(bs['kpoint_branches']):
+                for sub_ind, ind in enumerate(branch):
+                    kpt = bs['kpoint_path'][ind]
+                    for label, point in path_labels.items():
+                        if np.allclose(point, kpt):
+                            print(kpt, point, label)
+                            if ind-branch_ind not in labelled:
+                                label = label.replace('GAMMA', '\Gamma')
+                                label = label.replace('SIGMA', '\Sigma')
+                                if sub_ind == len(branch)-1:
+                                    if branch_ind < len(bs['kpoint_branches'])-1:
+                                        next_point = bs['kpoint_path'][bs['kpoint_branches'][branch_ind+1][0]]
+                                        for new_label, new_point in path_labels.items():
+                                            new_label = new_label.replace('GAMMA', '\Gamma')
+                                            new_label = new_label.replace('SIGMA', '\Sigma')
+                                            if np.allclose(new_point, next_point):
+                                                label = '{}|{}'.format(label, new_label)
+                                                labelled.append(ind-branch_ind)
+                                                shear_planes.append(path[ind-branch_ind])
+                                label = '${}$'.format(label)
+                                xticklabels.append(label)
+                                xticks.append(path[ind-branch_ind])
+                                break
+        else:
+            if np.allclose(next_point, [0, 0, 0]):
+                label = '$\Gamma$'
+                xticklabels.append(label)
+                xticks.append(path[ind-branch_ind])
 
         ax_bs.set_xticks(xticks)
         ax_bs.set_xticklabels(xticklabels)
-        for plane in shear_planes:
-            ax_bs.axvline(plane, ls='-.', lw=1, c='k')
 
     if dos:
         dos_data = np.loadtxt(seed + '.adaptive.dat')
         energies = dos_data[:, 0]
         dos = dos_data[:, 1]
-        ax_dos.set_ylim(plot_window)
-        ax_dos.axvline(0, c='k')
-        ax_dos.set_yticks([])
         max_density = np.max(dos[np.where(energies > plot_window[0])])
-        ax_dos.set_xticks([])
-        ax_dos.set_xlim(0, max_density*1.2)
-        ax_dos.axhline(0, c='grey', ls='--', lw=1)
+        if bandstructure:
+            ax_dos.set_xticks([0.6*max_density])
+            ax_dos.set_xticklabels(['DOS'])
+            ax_dos.axhline(0, c='grey', ls='--', lw=1)
+            ax_dos.set_xlim(0, max_density*1.2)
+            ax_dos.set_ylim(plot_window)
+            ax_dos.axvline(0, c='k')
+            ax_dos.set_yticks([])
+            ax_dos.plot(dos, energies, lw=1, c='k')
+            ax_dos.fill_betweenx(energies[np.where(energies <= 0)], 0, dos[np.where(energies <= 0)], facecolor=valence, alpha=0.5)
+            ax_dos.fill_betweenx(energies[np.where(energies >= 0)], 0, dos[np.where(energies >= 0)], facecolor=conduction, alpha=0.5)
+        else:
+            ax_dos.set_ylabel('DOS')
+            ax_dos.set_xlabel('Energy (eV)')
+            ax_dos.axvline(0, c='grey', ls='--', lw=1)
+            ax_dos.set_ylim(0, max_density*1.2)
+            ax_dos.set_xlim(plot_window)
+            ax_dos.axhline(0, c='k')
+            ax_dos.plot(energies, dos, lw=1, c='k')
+            ax_dos.fill_between(energies[np.where(energies <= 0)], 0, dos[np.where(energies <= 0)], facecolor=valence, alpha=0.5)
+            ax_dos.fill_between(energies[np.where(energies >= 0)], 0, dos[np.where(energies >= 0)], facecolor=conduction, alpha=0.5)
         ax_dos.grid('off')
-        ax_dos.plot(dos, energies, lw=1, c='k')
-        ax_dos.fill_betweenx(energies[np.where(energies < 0)], 0, dos[np.where(energies < 0)], facecolor=valence, alpha=0.5)
-        ax_dos.fill_betweenx(energies[np.where(energies >= 0)], 0, dos[np.where(energies >= 0)], facecolor=conduction, alpha=0.5)
 
     if kwargs.get('pdf'):
         plt.savefig(seed.replace('.bands', '') + '_spectral.pdf', bbox_inches='tight')
