a1eef2917caf94b558de7537c2c75cf51da72c82
Matthew Evans <me388@cam.ac.uk>
2017-12-30 16:41:42 +0000

    Barebones crystal class updates

--- matador/crystal.py
+++ matador/crystal.py
@@ -1,22 +1,20 @@
+#!/usr/bin/env python
 # coding: utf-8
 """ This file implements the Crystal class, a wrapper
 to the raw dictionary stored in MongoDB that allows for validation,
 manipulation and analysis of the lattice.
 """
 from matador.similarity.pdf_similarity import PDF
+from matador.utils.cell_utils import cart2abc, cart2volume
 
 
 class Crystal(object):
     """ Class that wraps the MongoDB document, providing useful
     interfaces for cell manipulation and validation.
     """
-    def __init__(self, doc, a, b, d):
+    def __init__(self, doc):
         self.doc = doc
         self.__dict__.update(self.doc)
-        self.a = a
-        self.b = b
-        self.d = d
-        # self.__c = None
 
     def __getitem__(self, key):
         if key not in self.doc:
@@ -26,47 +24,38 @@ class Crystal(object):
 
     def __setitem__(self, key, item):
         self.doc[key] = item
+
     @property
-    def c(self):
-        print('Recomputing c..')
-        return self.a + self.b
+    def lattice_cart(self):
+        return self['lattice_cart']
+
+    @property
+    def lattice_abc(self):
+        if 'lattice_abc' not in self.__dict__:
+            return cart2abc(self['lattice_cart'])
+        else:
+            return self['lattice_abc']
+
+    @property
+    def cell_volume(self):
+        if 'cell_volume' not in self.__dict__:
+            print('Recomputing cell volume')
+            return cart2volume(self.lattice_cart)
+        else:
+            return self['cell_volume']
+
     @property
     def pdf(self, **kwargs):
-        print('Recomputing pdf..')
-        self.doc['pdf'] = PDF(self.doc, **kwargs)
+        self.doc['pdf'] = PDF(self.doc, dr=0.01, gaussian_width=0.01, **kwargs)
         return self.doc['pdf']
-    # @c.setter
-    # def c(self, **kwargs):
-        # print('Recomputing c..')
-        # self.__c = self.a + self.b
+
 
 if __name__ == '__main__':
-    from matador.query import DBQuery
-    doc = DBQuery(formula='Li3P', top=1).cursor[0]
-    cryst = Crystal(doc, 5, 3, 10)
-    print(cryst.c)
-    print(cryst['pdf'])
-    # cryst.pdf.plot_pdf()
-    print(cryst['lattice_cart'])
-    print(cryst.lattice_cart)
 
-    cryst.lattice_cart[0][0] = cryst.lattice_cart[0][0] * 0.5
-    print(cryst['lattice_cart'])
-    cryst['lattice_cart'][0][0] = cryst.lattice_cart[0][0] * 0.5
-    print(cryst.lattice_cart)
-    print(cryst['pdf'])
-    print(cryst.geom_method)
-    cryst.geom_method = 'lbfgs'
-    print(cryst['geom_method'])
-    # cryst.d += 1
-    # print(cryst.c)
-    # print(cryst.pdf)
-    # assert cryst['geom_method'] == 'lbfgs'
-    # print(cryst.pdf)
-    # print(cryst['lattice_cart'])
-    # # cryst['pdf'].plot_pdf()
-    # print(cryst.pdf)
-    # cryst['lattice_cart'][0][0] = cryst['lattice_cart'][0][0] + 10
-    # print(cryst['lattice_cart'])
-    # print(cryst['pdf'])
-    # cryst['pdf'].plot_pdf()
+    from matador.query import DBQuery
+    doc = DBQuery(composition=['LiP'], top=1).cursor[0]
+    crystal = Crystal(doc)
+    print(crystal.cell_volume)
+    del crystal.__dict__['cell_volume']
+    print(crystal.cell_volume)
+    print(crystal['cell_volume'])
