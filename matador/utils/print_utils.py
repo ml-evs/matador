388f062112629e0b4081c0e897ab9671cbe5a679
Matthew Evans <me388@cam.ac.uk>
2018-01-30 16:10:45 +0000

    Adjusted print utils to write warnings/errors to stderr

--- matador/utils/print_utils.py
+++ matador/utils/print_utils.py
@@ -1,19 +1,18 @@
 """ This file implements some useful
 wrappers to the print function.
 """
-
-from __future__ import print_function
+import sys
 
 
 def print_warning(string):
     print('\033[93m', end='')
-    print(string, end='')
+    print(string, end='', file=sys.stderr)
     print('\033[0m')
 
 
 def print_failure(string):
     print('\033[91m\033[4m', end='')
-    print(string, end='')
+    print(string, end='', file=sys.stderr)
     print('\033[0m')
 
 
