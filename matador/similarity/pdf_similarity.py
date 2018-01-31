Similarities/uniqueness now also calculated in parallel, also fixed missing factor 2 in the hetero component of element-projected pdfs

matador/similarity/pdf_similarity.py
@@ -70,10 +70,15 @@ class PDF:
             if self.timing:
                 end = time.time()
                 print('PDF calculated in {:.3f} s'.format(end - start))
-            if kwargs.get('projected'):
-                self._calc_projected_pdf()
 
-    def _calc_distances(self, poscart, poscart_B=None, debug=False):
+    def calc_pdf(self):
+        """ Wrapper to calculate PDF with current settings. """
+        self._set_image_trans_vectors()
+        if self.projected:
+            self._calc_projected_pdf()
+        self._calc_unprojected_pdf()
+
+    def _calc_distances(self, poscart, poscart_B=None):
         """ Calculate PBC distances with cdist.
 
         Input:
@@ -105,7 +110,7 @@ class PDF:
         # mask by rmax/0 and remove masked values
         distances = np.ma.masked_where(distances > self.rmax, distances)
         distances = np.ma.masked_where(distances < 1e-12, distances)
-        if debug:
+        if self.debug:
             print('Calculated: {}, Used: {}, Ignored: {}'.format(len(distances),
                                                                  np.ma.count(distances),
                                                                  np.ma.count_masked(distances)))
@@ -117,7 +122,7 @@ class PDF:
 
         return self.distances
 
-    def calc_pdf(self, debug=False):
+    def _calc_unprojected_pdf(self):
         """ Wrapper function to calculate distances and output
         a broadened and normalised PDF.
 
@@ -126,12 +131,20 @@ class PDF:
             | Sets self.Gr and self.r_space to G(r) and r respectively.
 
         """
-        self._set_image_trans_vectors()
-        distances = self._calc_distances(self.poscart, debug=self.debug)
-        self.r_space = np.arange(0, self.rmax+self.dr, self.dr)
-        self.Gr = self._set_broadened_normalised_pdf(distances,
-                                                     style=self.style,
-                                                     gaussian_width=self.gaussian_width)
+        if 'elem_Gr' in self.__dict__:
+            self._calc_unprojected_pdf_from_projected()
+        else:
+            distances = self._calc_distances(self.poscart)
+            self.r_space = np.arange(0, self.rmax+self.dr, self.dr)
+            self.Gr = self._set_broadened_normalised_pdf(distances,
+                                                         style=self.style,
+                                                         gaussian_width=self.gaussian_width)
+
+    def _calc_unprojected_pdf_from_projected(self):
+        """" Reconstruct full PDF from projected. """
+        self.Gr = np.zeros_like(self.r_space)
+        for key in self.elem_Gr:
+            self.Gr += self.elem_Gr[key]
 
     def _set_broadened_normalised_pdf(self, distances, style='smear', gaussian_width=0.01):
         """ Broaden the values provided as distances and return
@@ -140,6 +153,9 @@ class PDF:
         Input:
 
             | distances      : np.ndarray, used to calculate PDF
+
+        Args:
+
             | style          : str, either 'smear' or 'histogram'
             | gaussian_width : float, smearing width in Angstrom^1/2
 
@@ -162,22 +178,27 @@ class PDF:
             hist[ceil(d_ij/self.dr)] += 1
         if style == 'histogram' or gaussian_width == 0:
             # if hist, normalise G(r) by ideal gas then be done
-            Gr = np.divide(hist,
-                           4*np.pi * (self.r_space + self.dr)**2 * self.dr * self.num_atoms * self.number_density)
-        elif style == 'smear':
-            if not self.low_mem:
+            norm = 4*np.pi * (self.r_space + self.dr)**2 * self.dr * self.num_atoms * self.number_density
+            Gr = np.divide(hist, norm)
+        # otherwise do normal smearing
+        else:
+            if self.low_mem:
+                if self.debug:
+                    print('Using low memory mode...')
+                new_space = np.reshape(self.r_space, (1, len(self.r_space))) - np.reshape(self.distances, (1, len(self.distances))).T
+                Gr = np.sum(np.exp(-(new_space)**2 / gaussian_width), axis=0)
+            else:
                 try:
-                    # otherwise, stack some Gaussians on that PDF
                     new_space = np.reshape(self.r_space, (1, len(self.r_space))) - np.reshape(self.r_space, (1, len(self.r_space))).T
                     Gr = np.sum(hist*np.exp(-(new_space)**2 / gaussian_width), axis=1)
                 except MemoryError:
-                    print('Memory usage too high; consider decreasing dr or turning on low_mem mode.')
-            else:
-                if self.debug:
-                    print('Using low memory mode...')
-                # if run out of memory, use low memory mode
-                new_space = np.reshape(self.r_space, (1, len(self.r_space))) - np.reshape(self.distances, (1, len(self.distances))).T
-                Gr = np.sum(np.exp(-(new_space)**2 / gaussian_width), axis=0)
+                    # if run out of memory, use low memory mode
+                    if self.debug:
+                        print('Ran out of memory, using low memory mode...')
+                    self.low_mem = True
+                    new_space = np.reshape(self.r_space, (1, len(self.r_space))) - np.reshape(self.distances, (1, len(self.distances))).T
+                    Gr = np.sum(np.exp(-(new_space)**2 / gaussian_width), axis=0)
+
             # normalise G(r) by Gaussian integral and then ideal gas
             Gr = np.divide(Gr,
                            np.sqrt(np.pi * gaussian_width) *
@@ -204,19 +225,21 @@ class PDF:
         """
         # initalise dict of element pairs with correct keys
         self.r_space = np.arange(0, self.rmax+self.dr, self.dr)
-        self.elem_Gr = dict()
+        elem_Gr = dict()
         for comb in combinations_with_replacement(set(self.doc['atom_types']), 2):
-            self.elem_Gr[tuple(set(comb))] = np.zeros_like(self.r_space)
+            elem_Gr[tuple(set(comb))] = np.zeros_like(self.r_space)
 
         distances = dict()
-        for elem_type in self.elem_Gr:
+        for elem_type in elem_Gr:
             poscart = [self.poscart[i] for i in range(len(self.poscart)) if self.doc['atom_types'][i] == elem_type[0]]
             poscart_B = ([self.poscart[i] for i in range(len(self.poscart)) if self.doc['atom_types'][i] == elem_type[1]]
                          if len(elem_type) == 2 else None)
             distances[elem_type] = self._calc_distances(poscart, poscart_B=poscart_B)
+            elem_Gr[elem_type] = len(elem_type)* self._set_broadened_normalised_pdf(distances[elem_type],
+                                                                                    style=self.style,
+                                                                                    gaussian_width=self.gaussian_width)
 
-            self.elem_Gr[elem_type] = self._set_broadened_normalised_pdf(distances[elem_type],
-                                                                         gaussian_width=self.gaussian_width)
+        self.elem_Gr = elem_Gr
 
     def _set_image_trans_vectors(self):
         """ Sets self.image_vec to a list/generator of image translation vectors,
@@ -269,9 +292,10 @@ class PDF:
                         print(cart2abc(self.doc['lattice_cart']))
                     break
         else:
-            self.image_vec = product(range(-self.num_images, self.num_images+1), repeat=3)
+            self.image_vec = list(product(range(-self.num_images, self.num_images+1), repeat=3))
         if self.debug:
             end = time.time()
+            print('Image vectors length = {}'.format(len(self.image_vec)))
             print('Set image trans vectors in {} s'.format(end-start))
 
     def get_sim_distance(self, pdf_B, projected=False):
@@ -295,6 +319,16 @@ class PDF:
             keys = [key for key in self.elem_Gr]
         for key in keys:
             ax1.plot(self.r_space, self.elem_Gr[key], label='-'.join(key))
+        if other_pdfs is not None:
+            if isinstance(other_pdfs, PDF):
+                other_pdfs = [other_pdfs]
+            for pdf in other_pdfs:
+                if isinstance(pdf, PDF):
+                    ax1.plot(pdf.r_space, pdf.Gr, lw=2, label=pdf.label, alpha=1, ls='--')
+                elif isinstance(pdf, tuple):
+                    ax1.plot(pdf[0], pdf[1], lw=2, alpha=1, ls='--')
+                else:
+                    raise RuntimeError
         ax1.legend(loc=1)
         ax1.set_ylabel('$g(r)$')
         ax1.set_xlabel('$r$ (Angstrom)')
@@ -302,7 +336,7 @@ class PDF:
         return
 
     def plot_pdf(self, other_pdfs=None):
-        """ Plot projected PDFs, with optional list of
+        """ Plot PDFs, with optional list of
         tuples [(r_space, Gr), ...] of other PDFs.
         """
         import matplotlib.pyplot as plt
@@ -313,16 +347,18 @@ class PDF:
         fig = plt.figure(figsize=(8, 5))
         ax1 = fig.add_subplot(111)
         ax1.plot(self.r_space, self.Gr, lw=2, label=self.label)
-        if other_pdfs is not None:
-            for pdf in other_pdfs:
-                if isinstance(pdf, PDF):
-                    ax1.plot(pdf.r_space, pdf.Gr, lw=2, label=pdf.label)
-                elif isinstance(pdf, tuple):
-                    ax1.plot(pdf[0], pdf[1], lw=2)
-                else:
-                    raise RuntimeError
         ax1.set_ylabel('Pair distribution function, $g(r)$')
         ax1.set_xlim(0, self.rmax)
+        if other_pdfs is not None:
+            if isinstance(other_pdfs, PDF):
+                other_pdfs = [other_pdfs]
+            for pdf in other_pdfs:
+                if isinstance(pdf, PDF):
+                    ax1.plot(pdf.r_space, pdf.Gr, lw=2, label=pdf.label, alpha=1, ls='--')
+                elif isinstance(pdf, tuple):
+                    ax1.plot(pdf[0], pdf[1], lw=2, alpha=1, ls='--')
+                else:
+                    raise RuntimeError
         ax1.set_xlabel('$r$ (Angstrom)')
         try:
             sns.despine()
@@ -359,6 +395,7 @@ class PDFFactory:
             del pdf_args['lazy']
         for doc in cursor:
             doc['pdf'] = PDF(doc, lazy=True, **pdf_args)
+
         # how many processes to use? either SLURM_NTASKS, OMP_NUM_THREADS or total num CPUs
         if os.environ.get('SLURM_NTASKS') is not None:
             self.nprocs = int(os.environ.get('SLURM_NTASKS'))
@@ -413,13 +450,10 @@ class PDFFactory:
             pool.join()
 
         assert len(pdf_cursor) == len(cursor)
-
         for ind, doc in enumerate(cursor):
-            doc['pdf'] = pdf_cursor[ind]['pdf']
+            cursor[ind]['pdf'] = pdf_cursor[ind]['pdf']
 
-        cursor = pdf_cursor
         elapsed = time.time() - start
-
         if debug:
             print('Compute time: {:.4f} s'.format(elapsed))
             print('Work complete!')

matador/similarity/similarity.py
@@ -3,7 +3,7 @@
 structural similarity metrics.
 """
 # matador modules
-from matador.similarity.pdf_similarity import PDF
+from matador.similarity.pdf_similarity import PDF, PDFFactory
 from matador.utils.cursor_utils import get_array_from_cursor, get_guess_doc_provenance
 # external libraries
 import numpy as np
@@ -37,6 +37,8 @@ def get_uniq_cursor(cursor, sim_calculator=PDF, sim_tol=5e-2, energy_tol=1e-2,
         | sim_mat             : the correlation matrix of pair similarity distances
 
     """
+    import progressbar
+    bar = progressbar.ProgressBar(term_width=80)
     fingerprint_list = []
     if not enforce_same_stoich:
         energy_tol = 1e20
@@ -45,35 +47,24 @@ def get_uniq_cursor(cursor, sim_calculator=PDF, sim_tol=5e-2, energy_tol=1e-2,
     print('Calculating fingerprints...')
     if debug:
         import time
-    try:
-        import progressbar
-        bar = progressbar.ProgressBar(term_width=80)
-    except(ImportError):
-        def bar(cursor):
-            return cursor
-    for doc in bar(cursor):
-        if debug:
-            start = time.time()
-        fingerprint_list.append(sim_calculator(doc, **sim_calc_args))
-        if debug:
-            completed = time.time() - start
-            print('PDF of {} completed in {:0.1f} s'.format(' '.join(doc['text_id']), completed))
+        start = time.time()
+    PDFFactory(cursor, **sim_calc_args)
+    # for doc in bar(cursor):
+        # fingerprint_list.append(sim_calculator(doc, **sim_calc_args))
+    if debug:
+        completed = time.time() - start
+        print('PDFs of {} structures completed in {:0.1f} s'.format(len(cursor), completed))
 
+    fingerprint_list = [doc['pdf'] for doc in cursor]
     sim_mat = np.ones((len(fingerprint_list), len(fingerprint_list)))
     print('Assessing similarities...')
-    try:
-        import progressbar
-        bar = progressbar.ProgressBar(term_width=80)
-    except(ImportError):
-        pass
     for i in bar(range(len(fingerprint_list))):
         sim_mat[i, i] = 0
         for j in range(i+1, len(fingerprint_list)):
             # are we checking stoichiometries, if so, ensure they're the same
             if (enforce_same_stoich is False or
-                (sorted(cursor[j]['stoichiometry']) == sorted(cursor[i]['stoichiometry'])
-                 and np.abs(cursor[j]['enthalpy_per_atom']
-                            - cursor[i]['enthalpy_per_atom']) < energy_tol)):
+                (sorted(cursor[j]['stoichiometry']) == sorted(cursor[i]['stoichiometry']) and
+                 np.abs(cursor[j]['enthalpy_per_atom'] - cursor[i]['enthalpy_per_atom']) < energy_tol)):
                 sim = fingerprint_list[i].get_sim_distance(fingerprint_list[j], projected=projected)
                 sim_mat[i, j] = sim
                 sim_mat[j, i] = sim

matador/tests/pdf_test.py
@@ -48,9 +48,18 @@ class PDFCalculatorTest(unittest.TestCase):
         doc, success = res2dict(REAL_PATH + 'data/LiPZn-r57des.res')
         doc['lattice_cart'] = abc2cart(doc['lattice_abc'])
         doc['text_id'] = ['pdf', 'test']
-        doc['pdf_num_images'] = PDF(doc, num_images=5, **{'debug': False})
-        doc['pdf_auto_images'] = PDF(doc, num_images='auto', **{'debug': False})
+        doc['pdf_num_images'] = PDF(doc, num_images=5, **{'debug': False, 'projected': False})
+        doc['pdf_auto_images'] = PDF(doc, num_images='auto', **{'debug': False, 'projected': False})
         np.testing.assert_array_almost_equal(doc['pdf_num_images'].Gr, doc['pdf_auto_images'].Gr)
+
+    def testPDFFromProjected(self):
+        doc, success = res2dict(REAL_PATH + 'data/LiPZn-r57des.res')
+        doc['lattice_cart'] = abc2cart(doc['lattice_abc'])
+        doc['text_id'] = ['unprojected', 'test']
+        doc['pdf_unprojected'] = PDF(doc, dr=0.01, **{'debug': True})
+        doc['text_id'] = ['projected', 'test']
+        doc['pdf_projected'] = PDF(doc, dr=0.01, **{'debug': True})
+        np.testing.assert_array_almost_equal(doc['pdf_unprojected'].Gr, doc['pdf_projected'].Gr)
 
     def testOverlapPDFSameStructure(self):
         doc, success = res2dict(REAL_PATH + 'data/LiPZn-r57des.res')
@@ -63,9 +72,11 @@ class PDFCalculatorTest(unittest.TestCase):
     def testOverlapPDFHistVSSmear(self):
         doc, success = res2dict(REAL_PATH + 'data/LiPZn-r57des.res')
         doc['lattice_cart'] = abc2cart(doc['lattice_abc'])
-        doc['text_id'] = ['pdf', 'test']
-        doc['pdf_smear'] = PDF(doc, num_images=3, dr=0.001, gaussian_width=0.01, style='smear', low_mem=True)
-        doc['pdf_hist'] = PDF(doc, num_images=3, dr=0.1, style='histogram')
+        doc['text_id'] = ['smear', 'test']
+        doc['pdf_smear'] = PDF(doc, num_images=3, dr=0.001, gaussian_width=0.01, projected=False, style='smear', low_mem=True)
+        doc['text_id'] = ['hist', 'test']
+        doc['pdf_hist'] = PDF(doc, num_images=3, dr=0.1, projected=False, style='histogram')
+        doc['pdf_smear'].plot_pdf(other_pdfs=doc['pdf_hist'])
         overlap = PDFOverlap(doc['pdf_smear'], doc['pdf_hist'])
         self.assertLessEqual(overlap.similarity_distance, 0.02)
         self.assertGreater(overlap.similarity_distance, 0.0)
@@ -85,27 +96,54 @@ class PDFCalculatorTest(unittest.TestCase):
         self.assertLessEqual(overlap.similarity_distance, 1e-3)
         self.assertGreater(overlap.similarity_distance, 0.0)
 
+
+class PDFFactoryCalculatorTest(unittest.TestCase):
+    """ Test PDFFactory. """
     def testPDFFactoryConcurrentPDFs(self):
         import glob
         import numpy as np
         import time
         from copy import deepcopy
-        files = glob.glob(REAL_PATH + 'data/hull-KPSn-KP/*.res')[0:48]
+        files = glob.glob(REAL_PATH + 'data/hull-KPSn-KP/*.res')[0:24]
         cursor = [res2dict(file, db=False)[0] for file in files]
         serial_cursor = deepcopy(cursor)
-        pdf_args = {'dr': 0.01, 'num_images': 'auto', 'gaussian_width': 0.1, 'lazy': False}
+        pdf_args = {'dr': 0.1, 'num_images': 'auto', 'gaussian_width': 0.1, 'lazy': False, 'projected': False}
         start = time.time()
         pdf_factory = PDFFactory(cursor, concurrency='pool', **pdf_args)
         factory_elapsed = time.time() - start
         start = time.time()
         for doc in serial_cursor:
-                doc['pdf'] = PDF(doc, **pdf_args, timing=False)
+            doc['pdf'] = PDF(doc, **pdf_args, timing=False)
         serial_elapsed = time.time() - start
-        print('{:.2f} s over 6 processes vs {:.2f} s in serial'.format(factory_elapsed, serial_elapsed))
+        print('{:.2f} s over {} processes vs {:.2f} s in serial'.format(factory_elapsed, pdf_factory.nprocs, serial_elapsed))
         print('Corresponding to a speedup of {:.1f} vs ideal {:.1f}'.format(serial_elapsed/factory_elapsed,
                                                                             pdf_factory.nprocs))
         for ind, doc in enumerate(serial_cursor):
             np.testing.assert_array_almost_equal(doc['pdf'].Gr, cursor[ind]['pdf'].Gr, decimal=6)
+
+    def testPDFFactoryConcurrentProjectedPDFs(self):
+        import glob
+        import numpy as np
+        import time
+        from copy import deepcopy
+        files = glob.glob(REAL_PATH + 'data/hull-KPSn-KP/*.res')[0:24]
+        cursor = [res2dict(file, db=False)[0] for file in files]
+        serial_cursor = deepcopy(cursor)
+        pdf_args = {'dr': 0.1, 'num_images': 'auto', 'gaussian_width': 0.1, 'lazy': False, 'projected': True}
+        start = time.time()
+        pdf_factory = PDFFactory(cursor, concurrency='pool', **pdf_args)
+        factory_elapsed = time.time() - start
+        start = time.time()
+        for doc in serial_cursor:
+            doc['pdf'] = PDF(doc, **pdf_args, timing=False)
+        serial_elapsed = time.time() - start
+        print('{:.2f} s over {} processes vs {:.2f} s in serial'.format(factory_elapsed, pdf_factory.nprocs, serial_elapsed))
+        print('Corresponding to a speedup of {:.1f} vs ideal {:.1f}'.format(serial_elapsed/factory_elapsed,
+                                                                            pdf_factory.nprocs))
+        for ind, doc in enumerate(serial_cursor):
+            np.testing.assert_array_almost_equal(doc['pdf'].Gr, cursor[ind]['pdf'].Gr, decimal=6)
+            for key in doc['pdf'].elem_Gr:
+                np.testing.assert_array_almost_equal(doc['pdf'].elem_Gr[key], cursor[ind]['pdf'].elem_Gr[key], decimal=6)
 
 
 if __name__ == '__main__':