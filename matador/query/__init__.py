2221ceaaae437e10eef916c32121014576145f6e
Matthew Evans <me388@cam.ac.uk>
2018-05-20 17:01:07 +0100

    MAINT: moved query to its own module

---
+++ matador/query/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# Distributed under the terms of the MIT License.
+
+""" The query module provides the DBQuery class that implements all
+database queries, either for individual compositions/formulae, or for
+matching calculations at the same accuracy to create phase diagrams.
+
+"""
+
+
+__all__ = ['DBQuery']
+__author__ = 'Matthew Evans'
+__maintainer__ = 'Matthew Evans'
+
+
+from matador.query.query import DBQuery
