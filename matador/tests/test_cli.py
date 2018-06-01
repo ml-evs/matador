a2c9a8dba0aba886f23590fcdf3cb85efc451e6c
Matthew Evans <me388@cam.ac.uk>
2018-06-01 01:28:08 +0100

    TST: updated tests to reflect new changes

--- matador/tests/test_cli.py
+++ matador/tests/test_cli.py
@@ -29,17 +29,17 @@ try:
 except:
     pass
 
-files_to_delete = glob.glob('*spatula*')
-for f in files_to_delete:
-    os.remove(f)
+for _file in glob.glob('*spatula*'):
+    os.remove(_file)
 
 
 @unittest.skipIf(not MONGO_PRESENT, 'MongoDB instance not found, skipping tests...')
-class CLITest(unittest.TestCase):
+class CLIIntegrationTest(unittest.TestCase):
     """ Test CLI functionality. """
-    def testImporter(self):
+    def testImporterAndMore(self):
         """ Test import and query. """
         from matador.query import DBQuery
+        from matador.scrapers.castep_scrapers import cell2dict, res2dict
         os.chdir(REAL_PATH + '/data/castep_files')
         sys.argv = ['matador', 'import', '--db', DB_NAME]
 
@@ -55,6 +55,53 @@ class CLITest(unittest.TestCase):
         self.assertEqual(len(files_to_delete), 2)
         for f in files_to_delete:
             os.remove(f)
+
+        os.chdir(REAL_PATH)
+
+        sys.argv = ['matador', 'swaps', '--db', DB_NAME, '--res', '--cell',
+                    '-c', 'NaPZn', '-int', '-sw', 'NaLi:PSi:ZnSn']
+
+        if CONFIG_FNAME is not None:
+            sys.argv += ['--config', CONFIG_FNAME]
+
+        matador.cli.cli.main()
+
+        expected_dir = 'swaps-NaPZn-ci_test-NaLi:PSi:ZnSn'
+        output_folder_exists = os.path.isdir(expected_dir)
+        print(output_folder_exists)
+        successes = []
+        elem_successes = []
+
+        if output_folder_exists:
+            os.chdir(expected_dir)
+            expected_files = ['LiSi-swap-NaP_intermediates', 'LiSn-swap-Na3Zn4-OQMD_759599', 'Li-swap-Na-edgecase']
+            target_elems = ['Li', 'Si', 'Sn']
+
+            for files in expected_files:
+                cell, s = cell2dict(files, db=False, positions=True)
+                successes.append(s)
+                if s:
+                    elems = set(cell['atom_types'])
+                    elem_successes.append(any([elem not in target_elems for elem in elems]))
+                else:
+                    elem_successes.append(False)
+                res, s = res2dict(files, db=False)
+                successes.append(s)
+                if s:
+                    elems = set(res['atom_types'])
+                    elem_successes.append(any([elem not in target_elems for elem in elems]))
+                else:
+                    elem_successes.append(False)
+
+                os.remove(files + '.cell')
+                os.remove(files + '.res')
+
+            os.chdir(REAL_PATH)
+            os.rmdir(expected_dir)
+
+        self.assertTrue(output_folder_exists, msg='No folder created')
+        self.assertTrue(all(successes), msg='Failed to even read files')
+        self.assertFalse(all(elem_successes), msg='Swaps had wrong elements')
 
 
 if __name__ == '__main__':
