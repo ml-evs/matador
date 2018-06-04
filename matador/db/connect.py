1f72e168ef2c9412126e5f854208e570d95e3ca8
Matthew Evans <me388@cam.ac.uk>
2018-06-04 15:19:35 +0100

    ENH: added fuzzy --db name matching when database doesn't exist

--- matador/db/connect.py
+++ matador/db/connect.py
@@ -60,12 +60,12 @@ def make_connection_to_collection(coll_names, check_collection=False, allow_chan
                              .format(**settings['mongo']))
 
         if response.lower() != 'y':
-            exit('Exiting...')
+            raise SystemExit('Exiting...')
         else:
             print('Creating database {}'.format(settings['mongo']['db']))
 
     db = client[settings['mongo']['db']]
-    possible_collections = db.collection_names()
+    possible_collections = [name for name in db.collection_names() if not name.startswith('__')]
     collections = dict()
     # allow lists of collections for backwards-compat, though normally
     # we only want to connect to one at a time
@@ -75,21 +75,63 @@ def make_connection_to_collection(coll_names, check_collection=False, allow_chan
         for collection in coll_names:
             if collection not in possible_collections:
                 if check_collection:
-                    client.close()
-                    exit('Collection {} not found!'.format(collection))
+                    options = fuzzy_collname_match(collection, possible_collections)
+                    if not options:
+                        client.close()
+                        raise SystemExit('Collection {} not found!'.format(collection))
+                    else:
+                        print('Collection not found, did you mean one of these?')
+                        for ind, value in enumerate(options[:20]):
+                            print('({}):\t{}'.format(ind, value))
+                        try:
+                            choice = int(input('Please enter your choice: '))
+                            collection = options[choice]
+                        except:
+                            raise SystemExit('Invalid choice. Exiting...')
                 else:
                     print('Creating new collection {}...'.format(collection))
             if not allow_changelog and collection.startswith('__'):
-                exit('Queries to collections prefixed with __ are VERBOTEN!')
+                raise SystemExit('Queries to collections prefixed with __ are VERBOTEN!')
             collections[collection] = db[collection]
     else:
         default_collection = settings['mongo']['default_collection']
         if default_collection not in possible_collections:
             if check_collection:
                 client.close()
-                exit('Default collection {} not found!'.format(default_collection))
+                raise SystemExit('Default collection {} not found!'.format(default_collection))
             else:
                 print('Creating new collection {}...'.format(default_collection))
         collections['repo'] = db[default_collection]
 
     return client, db, collections
+
+
+def fuzzy_collname_match(trial, targets):
+    """ Do a noddy fuzzy match for bits between punctuation, e.g.
+    matthews_cool_database will search for matthews, cool and database
+    in the known collection names.
+
+    Parameters:
+        trial (str): database search name.
+        targets (list): list of existing database names.
+
+    Returns:
+        list: list of roughly matching collection names ordered
+            by occurence of tokens.
+
+    """
+    split_chars = set([char for char in trial if (not char.isalpha() and not char.isdigit())])
+    tokens = trial
+    for char in split_chars:
+        tokens = tokens.replace(char, ' ')
+    tokens = tokens.split()
+    options = {}
+    for token in tokens:
+        for option in targets:
+            if token in option:
+                if option not in options:
+                    options[option] = 0
+                else:
+                    options[option] += 1
+    options_list = [item[0] for item in list(sorted(options.items(), reverse=True, key=lambda item: item[1]))]
+    return options_list
