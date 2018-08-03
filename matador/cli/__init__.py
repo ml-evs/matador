cdf97851d27aa91e8cb83d79964c1e8a754bf0ac
Matthew Evans <me388@cam.ac.uk>
2018-08-03 16:26:39 +0100

    FIX: fixed bug that caused run3 to depend on pymongo

--- matador/cli/__init__.py
+++ matador/cli/__init__.py
@@ -1,9 +1,5 @@
 # coding: utf-8
 # Distributed under the terms of the MIT License.
 
-__all__ = ['MatadorCommandLine']
 __author__ = 'Matthew Evans'
 __maintainer__ = 'Matthew Evans'
-
-
-from matador.cli.cli import MatadorCommandLine
