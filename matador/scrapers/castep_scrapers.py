FIX: fixed broken positions_abs scraper when units are present, improved SHELX scraper

matador/scrapers/castep_scrapers.py
@@ -8,13 +8,12 @@ inputs and outputs.
 
 
 from collections import defaultdict
-from os import stat
-from os.path import isfile
-from time import strptime
-from pwd import getpwuid
+import os
 import glob
 import gzip
-from matador.utils.cell_utils import abc2cart, calc_mp_spacing, cart2volume, wrap_frac_coords, cart2abc, frac2cart
+import pwd
+import numpy as np
+from matador.utils.cell_utils import abc2cart, calc_mp_spacing, cart2volume, wrap_frac_coords, cart2abc, frac2cart, cart2frac, real2recip
 from matador.utils.chem_utils import get_stoich
 from matador.scrapers.utils import DFTError, CalculationError, scraper_function, f90_float_parse
 
@@ -46,7 +45,7 @@ def res2dict(seed, db=True, **kwargs):
     res['source'] = []
     res['source'].append(seed + '.res')
     # grab file owner username
-    res['user'] = getpwuid(stat(seed + '.res').st_uid).pw_name
+    res['user'] = pwd.getpwuid(os.stat(seed + '.res').st_uid).pw_name
     get_seed_metadata(res, seed)
     # alias special lines in res file
     titl = ''
@@ -85,6 +84,11 @@ def res2dict(seed, db=True, **kwargs):
         if 'SFAC' in line:
             i = 1
             while 'END' not in flines[line_no + i] and line_no + i < len(flines):
+                # check if we don't have some other SHELX keyword in the way, e.g. "UNIT"
+                if(len(flines[line_no + i]) >= 4 and
+                   all([char.isupper() for char in flines[line_no+i][0:4]])):
+                    i += 1
+                    continue
                 cursor = flines[line_no + i].split()
                 res['atom_types'].append(cursor[0])
                 res['positions_frac'].append(list(map(f90_float_parse, cursor[2:5])))
@@ -120,7 +124,7 @@ def res2dict(seed, db=True, **kwargs):
 
 
 @scraper_function
-def cell2dict(seed, db=True, lattice=False, outcell=False, positions=False, **kwargs):
+def cell2dict(seed, db=False, lattice=True, positions=True, **kwargs):
     """ Extract available information from .cell file; probably
     to be merged with another dict from a .param or .res file.
 
@@ -139,8 +143,6 @@ def cell2dict(seed, db=True, lattice=False, outcell=False, positions=False, **kw
 
     """
     cell = dict()
-    if outcell:
-        lattice = True
     if seed.endswith('.cell'):
         seed = seed.replace('.cell', '')
     with open(seed + '.cell', 'r') as f:
@@ -321,19 +323,18 @@ def cell2dict(seed, db=True, lattice=False, outcell=False, positions=False, **kw
                 if any(atomic_init_spins):
                     cell['atomic_init_spins'] = atomic_init_spins
                 if positions:
+                    cell['positions_frac'] = wrap_frac_coords(cell['positions_frac'])
                     cell['num_atoms'] = len(cell['atom_types'])
-                    for ind, pos in enumerate(cell['positions_frac']):
-                        for k in range(3):
-                            if pos[k] > 1 or pos[k] < 0:
-                                cell['positions_frac'][ind][k] %= 1
             elif '%block positions_abs' in line.lower():
                 atomic_init_spins = []
                 i = 1
+                # avoid units
+                if len(flines[line_no+i].split()) < 3:
+                    i += 1
                 if positions:
                     cell['atom_types'] = []
                     cell['positions_abs'] = []
                 while '%endblock positions_abs' not in flines[line_no + i].lower():
-                    print(flines[line_no+i])
                     line = flines[line_no + i].split()
                     if positions:
                         cell['atom_types'].append(line[0])
@@ -346,12 +347,6 @@ def cell2dict(seed, db=True, lattice=False, outcell=False, positions=False, **kw
                     i += 1
                 if any(atomic_init_spins):
                     cell['atomic_init_spins'] = atomic_init_spins
-                if positions:
-                    cell['num_atoms'] = len(cell['atom_types'])
-                    for ind, pos in enumerate(cell['positions_abs']):
-                        for k in range(3):
-                            if pos[k] > 1 or pos[k] < 0:
-                                cell['positions_abs'][ind][k] %= 1
 
             elif 'fix_com' in line.lower():
                 cell['fix_com'] = line.split()[-1]
@@ -384,15 +379,17 @@ def cell2dict(seed, db=True, lattice=False, outcell=False, positions=False, **kw
         elif 'lattice_cart' in cell and 'lattice_abc' not in cell:
             cell['lattice_abc'] = cart2abc(cell['lattice_cart'])
         cell['cell_volume'] = cart2volume(cell['lattice_cart'])
+
     if positions:
         if 'positions_frac' not in cell:
-            cell['positions_frac'] = frac2cart(cell['lattice_cart'], cell['positions_abs'])
+            cell['positions_frac'] = cart2frac(cell['lattice_cart'], cell['positions_abs'])
+            cell['positions_frac'] = wrap_frac_coords(cell['positions_frac'])
 
     if db:
         for species in cell['species_pot']:
             if 'OTF' in cell['species_pot'][species].upper():
                 pspot_seed = '/'.join(seed.split('/')[:-1]) + '/' + cell['species_pot'][species]
-                if isfile(pspot_seed):
+                if os.path.isfile(pspot_seed):
                     cell['species_pot'].update(usp2dict(pspot_seed))
 
     return cell, True
@@ -559,7 +556,7 @@ def castep2dict(seed, db=True, intermediates=False, **kwargs):
     castep['source'] = []
     castep['source'].append(seed)
     # grab file owner
-    castep['user'] = getpwuid(stat(seed).st_uid).pw_name
+    castep['user'] = pwd.getpwuid(os.stat(seed).st_uid).pw_name
     get_seed_metadata(castep, seed.replace('.' + ftype, ''))
     # wrangle castep file for parameters in 3 passes:
     # once forwards to get number and types of atoms
@@ -605,7 +602,7 @@ def castep2dict(seed, db=True, intermediates=False, **kwargs):
                 # glob for all .usp files with format species_*OTF.usp
                 pspot_seed += castep['species_pot'][species]
                 for globbed in glob.glob(pspot_seed):
-                    if isfile(globbed):
+                    if os.path.isfile(globbed):
                         castep['species_pot'].update(usp2dict(globbed))
 
     # check that any optimized results were saved and raise errors if not
@@ -636,8 +633,6 @@ def bands2dict(seed, summary=False, gap=False, external_efermi=None, **kwargs):
 
     """
     from matador.utils.chem_utils import HARTREE_TO_EV, BOHR_TO_ANGSTROM
-    from matador.utils.cell_utils import real2recip
-    import numpy as np
 
     verbosity = kwargs.get('verbosity', 0)
 
@@ -839,7 +834,6 @@ def optados2dict(seed, **kwargs):
             if the scrape was successful.
 
     """
-    import numpy as np
     optados = dict()
     is_pdos = False
     is_pdis = False
@@ -962,8 +956,6 @@ def phonon2dict(seed, **kwargs):
             if the scrape was successful.
 
     """
-    import numpy as np
-    from matador.utils.cell_utils import frac2cart, real2recip
 
     with open(seed, 'r') as f:
         # read whole file into RAM, typically <~ 1 MB
@@ -1482,6 +1474,7 @@ def _castep_scrape_metadata(flines, castep):
         dict: dictionary updated with scraped data
 
     """
+    from time import strptime
     # computing metadata, i.e. parallelism, time, memory, version
     for ind, line in enumerate(flines):
         if 'Release CASTEP version' in line:
@@ -1746,7 +1739,6 @@ def get_kpt_branches(cart_kpts):
             - float: estimated kpoint spacing.
 
     """
-    import numpy as np
     kpt_branches = []
     kpts_diff = np.zeros((len(cart_kpts) - 1))
     kpts_diff_set = set()

matador/tests/test_file_io.py
@@ -23,7 +23,7 @@ class CellScraperTests(unittest.TestCase):
     def test_standard_cell_scraper(self):
         cell_fname = REAL_PATH + 'data/LiP2Zn-0bm995-a_9-out.cell'
         self.assertTrue(os.path.isfile(cell_fname), msg='Failed to open test case {} - please check installation.'.format(cell_fname))
-        test_dict, s = cell2dict(cell_fname, db=False, outcell=True, verbosity=VERBOSITY)
+        test_dict, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY)
         self.assertTrue(s, msg='Failed entirely, oh dear!\n{}'.format(test_dict))
         self.assertEqual(test_dict['lattice_cart'][0][0], 9.83262140721165, msg='Failed to read lattice vectors.')
         self.assertEqual(test_dict['lattice_cart'][1][1], 5.96357780025648, msg='Failed to read lattice vectors.')
@@ -35,19 +35,19 @@ class CellScraperTests(unittest.TestCase):
         self.assertEqual(test_dict['species_pot']['P'], 'P_00PBE.usp', msg='Failed to read pspots.')
         self.assertEqual(test_dict['species_pot']['Zn'], 'Zn_00PBE.usp', msg='Failed to read pspots.')
         # test that lattice_vec only read when outcell is true
-        test_dict, s = cell2dict(cell_fname, db=False, outcell=False, verbosity=VERBOSITY)
+        test_dict, s = cell2dict(cell_fname, db=False, lattice=False, verbosity=VERBOSITY)
         self.assertTrue(test_dict.get('lattice_cart') is None)
 
     def test_cell_outcell(self):
         cell_fname = REAL_PATH + 'data/Li2C2-out.cell'
         self.assertTrue(os.path.isfile(cell_fname), msg='Failed to open test case {} - please check installation.'.format(cell_fname))
-        test_dict, s = cell2dict(cell_fname, db=False, outcell=True, verbosity=VERBOSITY)
+        test_dict, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY)
         self.assertTrue(s, msg='Failed entirely, oh dear!\n{}'.format(test_dict))
 
     def test_cell_phonon(self):
         cell_fname = REAL_PATH + 'data/K5P4-phonon.cell'
         self.assertTrue(os.path.isfile(cell_fname), msg='Failed to open test case {} - please check installation.'.format(cell_fname))
-        test_dict, s = cell2dict(cell_fname, db=False, outcell=True, verbosity=VERBOSITY)
+        test_dict, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY)
         self.assertTrue(s, msg='Failed entirely, oh dear!\n{}'.format(test_dict))
         self.assertEqual(test_dict['lattice_cart'][0][0], 11.4518745146637, msg='Failed to read lattice vectors.')
         self.assertEqual(test_dict['lattice_cart'][1][1], 5.09448137301246, msg='Failed to read lattice vectors.')
@@ -100,6 +100,26 @@ class CellScraperTests(unittest.TestCase):
         self.assertEqual(cell['phonon_fine_kpoint_path'], [[0.0, 0.0, 0.0], [0.0, 0.0, 0.5], [0.0, 0.5, 0.0], [0.5, 0.0, 0.0]])
         self.assertEqual(cell['phonon_fine_kpoint_path_labels'], ['$\\Gamma$', 'Z', '$Y$', 'X'])
         self.assertEqual(cell['phonon_fine_kpoint_path_spacing'], 0.01)
+
+    def test_cell_positions_abs(self):
+        cell_name = REAL_PATH + 'data/cell_files/npm.cell'
+        cell, s = cell2dict(cell_name, db=False)
+
+        self.assertTrue(s, msg='Failed entirely: {}'.format(cell))
+        self.assertEqual(cell['lattice_cart'][0], [21.84, 0, 0])
+        self.assertEqual(cell['lattice_cart'][1], [0, 16.38, 0])
+        self.assertEqual(cell['lattice_cart'][2], [0, 0, 40.46])
+        print(cell['positions_abs'][95])
+
+        self.assertEqual(cell['positions_abs'][0], [0, 0, 0])
+        self.assertEqual(cell['positions_abs'][95], [17.745, 15.015, -4.095])
+        self.assertEqual(cell['positions_abs'][-1], [13.65, 13.65, 10.92])
+
+        np.testing.assert_array_almost_equal(cell['positions_frac'][0], [0, 0, 0])
+        np.testing.assert_array_almost_equal(cell['positions_frac'][95], [0.8125, 0.9166666, 0.8987889])
+        np.testing.assert_array_almost_equal(cell['positions_frac'][-1], [0.625, 0.8333333, 0.26989619])
+
+
 
 
 class CastepScraperTests(unittest.TestCase):
@@ -236,7 +256,7 @@ class CastepScraperTests(unittest.TestCase):
             f.close()
             test_dict, s = castep2dict(castep_fname, db=True, verbosity=VERBOSITY)
             self.assertFalse(s, msg='Should have failed with db=True, but didn\'t!')
-            self.assertTrue(isinstance(test_dict, str), msg='Should have returned error message!')
+            self.assertTrue(isinstance(test_dict, Exception), msg='Should have returned error message!')
             test_dict, s = castep2dict(castep_fname, db=False, verbosity=VERBOSITY)
             self.assertTrue(s, msg='Should have succeeded with db=False, but didn\'t!')
             self.assertTrue(isinstance(test_dict, dict), msg='Should have returned dict!')
@@ -414,43 +434,10 @@ class ResScraperTests(unittest.TestCase):
             test_dict, s = res2dict(res_fname)
             self.assertFalse(s, 'This wasn\'t meant to succeed!')
 
-    @unittest.skipIf(True, 'CIF tests temporarily disabled...')
-    def test_cif(self):
-        cif_fname = REAL_PATH + 'data/cif_files/AgBiI.cif'
-        failed_open = False
-        try:
-            f = open(cif_fname, 'r')
-        except Exception:
-            failed_open = True
-            self.assertFalse(failed_open, msg='Failed to open test case {} - please check installation.'.format(cif_fname))
-        if not failed_open:
-            f.close()
-            test_dict, s = cif2dict(cif_fname)
-            self.assertTrue(s, 'Failed entirely, oh dear! {}'.format(test_dict))
-            self.assertAlmostEqual(test_dict['num_atoms'], 46.623999999999995, msg='Failed to read num_atoms!', places=5)
-            self.assertTrue(['Bi', 1.0] in test_dict['stoichiometry'], msg='Wrong stoichiometry!')
-            self.assertTrue(['I', 4.0] in test_dict['stoichiometry'], msg='Wrong stoichiometry!')
-            self.assertTrue(sorted(test_dict['stoichiometry']) == test_dict['stoichiometry'], msg='Wrong stoichiometry!')
-            self.assertAlmostEqual(test_dict['cell_volume'], 1826.0028753, msg='Wrong cell volume!', places=3)
-            self.assertEqual(test_dict['space_group'], 'Fd-3m', msg='Wrong space group!')
-            self.assertEqual(len(test_dict['atom_types']), 64)
-            self.assertEqual(len(test_dict['positions_frac']), 64)
-            self.assertEqual(len(test_dict['site_occupancy']), 64)
-
-        cif_fname = REAL_PATH + 'data/cif_files/malicious.cif'
-        failed_open = False
-        try:
-            f = open(cif_fname, 'r')
-        except FileNotFoundError:
-            failed_open = True
-            self.assertFalse(failed_open, msg='Failed to open test case {} - please check installation.'.format(cif_fname))
-        if not failed_open:
-            f.close()
-            errored = False
-            test_dict, s = cif2dict(cif_fname)
-            errored = isinstance(test_dict, str) and 'RuntimeError' in test_dict
-            self.assertTrue(errored, 'WARNING: malicious attack is possible through symops')
-            self.assertFalse(s, 'This should have failed entirely, oh dear!')
+    def test_c2x_shelx_res(self):
+        res_fname = REAL_PATH + 'data/res_files/npm.res'
+        res, s = res2dict(res_fname, db=False)
+        self.assertTrue(s, msg='Failed entirely: {}'.format(res))
 
 
 class ParamScraperTests(unittest.TestCase):
@@ -860,6 +847,44 @@ class ScraperMiscTest(unittest.TestCase):
         pdis, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY)
         self.assertTrue(s)
 
+    @unittest.skipIf(True, 'CIF tests temporarily disabled...')
+    def test_cif(self):
+        cif_fname = REAL_PATH + 'data/cif_files/AgBiI.cif'
+        failed_open = False
+        try:
+            f = open(cif_fname, 'r')
+        except Exception:
+            failed_open = True
+            self.assertFalse(failed_open, msg='Failed to open test case {} - please check installation.'.format(cif_fname))
+        if not failed_open:
+            f.close()
+            test_dict, s = cif2dict(cif_fname)
+            self.assertTrue(s, 'Failed entirely, oh dear! {}'.format(test_dict))
+            self.assertAlmostEqual(test_dict['num_atoms'], 46.623999999999995, msg='Failed to read num_atoms!', places=5)
+            self.assertTrue(['Bi', 1.0] in test_dict['stoichiometry'], msg='Wrong stoichiometry!')
+            self.assertTrue(['I', 4.0] in test_dict['stoichiometry'], msg='Wrong stoichiometry!')
+            self.assertTrue(sorted(test_dict['stoichiometry']) == test_dict['stoichiometry'], msg='Wrong stoichiometry!')
+            self.assertAlmostEqual(test_dict['cell_volume'], 1826.0028753, msg='Wrong cell volume!', places=3)
+            self.assertEqual(test_dict['space_group'], 'Fd-3m', msg='Wrong space group!')
+            self.assertEqual(len(test_dict['atom_types']), 64)
+            self.assertEqual(len(test_dict['positions_frac']), 64)
+            self.assertEqual(len(test_dict['site_occupancy']), 64)
+
+        cif_fname = REAL_PATH + 'data/cif_files/malicious.cif'
+        failed_open = False
+        try:
+            f = open(cif_fname, 'r')
+        except FileNotFoundError:
+            failed_open = True
+            self.assertFalse(failed_open, msg='Failed to open test case {} - please check installation.'.format(cif_fname))
+        if not failed_open:
+            f.close()
+            errored = False
+            test_dict, s = cif2dict(cif_fname)
+            errored = isinstance(test_dict, RuntimeError) and 'RuntimeError' in test_dict
+            self.assertTrue(errored, 'WARNING: malicious attack is possible through symops')
+            self.assertFalse(s, 'This should have failed entirely, oh dear!')
+
 
 class ExportTest(unittest.TestCase):
     """ Test file export functions. """
@@ -900,9 +925,9 @@ class ExportTest(unittest.TestCase):
         for _f in glob.glob(REAL_PATH + 'data/dummy*.cell'):
             os.remove(_f)
 
-        doc, s = cell2dict(cell_fname, db=False, outcell=True, verbosity=VERBOSITY, positions=False)
+        doc, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY, positions=False)
         doc2cell(doc, test_fname, debug=True)
-        test_dict, s = cell2dict(test_fname, db=False, outcell=True, positions=False)
+        test_dict, s = cell2dict(test_fname, db=False, lattice=True, positions=False)
         self.assertTrue(s, msg='Failed entirely, oh dear!\n{}'.format(test_dict))
         self.assertEqual(test_dict['lattice_cart'][0][0], 11.4518745146637, msg='Failed to read lattice vectors.')
         self.assertEqual(test_dict['lattice_cart'][1][1], 5.09448137301246, msg='Failed to read lattice vectors.')
@@ -923,13 +948,13 @@ class ExportTest(unittest.TestCase):
         # test that overwrite overwrites
         doc['phonon_supercell_matrix'][2] = [0, 0, 140]
         doc2cell(doc, test_fname, overwrite=True)
-        test_dict, s = cell2dict(test_fname, db=False, outcell=True, positions=False)
+        test_dict, s = cell2dict(test_fname, db=False, lattice=True, positions=False)
         self.assertEqual(test_dict['phonon_supercell_matrix'][2], [0, 0, 140])
 
         # test that hash dupe doesn't
         doc['phonon_supercell_matrix'][2] = [0, 0, 333]
         doc2cell(doc, test_fname, overwrite=False, hash_dupe=True)
-        test_dict, s = cell2dict(test_fname, db=False, outcell=True, positions=False)
+        test_dict, s = cell2dict(test_fname, db=False, lattice=True, positions=False)
         self.assertEqual(test_dict['phonon_supercell_matrix'][2], [0, 0, 140])
 
         dummy_files = glob.glob(REAL_PATH + 'data/dummy*.cell')
@@ -958,13 +983,13 @@ class ExportTest(unittest.TestCase):
         test_fname = REAL_PATH + 'data/dummy3'
         test_param = {'xc_functional': 'PBE', 'task': 'geometryoptimisation', 'spin_polarized': False}
 
-        doc, s = cell2dict(cell_fname, db=False, outcell=True, verbosity=VERBOSITY, positions=True)
+        doc, s = cell2dict(cell_fname, db=False, lattice=True, verbosity=VERBOSITY, positions=True)
         doc2cell(doc, test_fname + '.cell', debug=True, spin=10)
 
         doc2param(test_param, test_fname + '.param', spin=10)
 
         param_doc, s = param2dict(test_fname + '.param')
-        cell_doc, s = cell2dict(test_fname + '.cell', db=False, outcell=True, positions=True)
+        cell_doc, s = cell2dict(test_fname + '.cell', db=False, lattice=True, positions=True)
 
         os.remove(test_fname + '.param')
         os.remove(test_fname + '.cell')