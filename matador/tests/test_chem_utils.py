FIX: made chempot linear solver numerically robust

matador/tests/test_chem_utils.py
@@ -72,7 +72,6 @@ class ChemUtilsTest(unittest.TestCase):
         conc = get_concentration(doc, ['Na', 'P'], include_end=True)
         self.assertEqual(conc, [0.75, 0.25])
 
-
     def test_volumetric_capacity(self):
         initial_doc = dict()
         final_doc = dict()
@@ -142,12 +141,8 @@ class ChemUtilsTest(unittest.TestCase):
         elements = ['F']
         self.assertEqual(get_padded_composition(stoich, elements), [8])
         elements = ['Fe', 'Ba', 'Ca']
-        errored = False
-        try:
+        with self.assertRaises(RuntimeError):
             get_padded_composition(stoich, elements)
-        except RuntimeError:
-            errored = True
-        self.assertTrue(errored)
 
     def test_formation_energy_binary(self):
         chempots = []
@@ -196,12 +191,8 @@ class ChemUtilsTest(unittest.TestCase):
         ef_400 = get_formation_energy(chempots, cursor[0], energy_key=['temperature', 'free_energy_per_atom', '400'])
         self.assertEqual(ef_400, -150)
 
-        errored = False
-        try:
+        with self.assertRaises(KeyError):
             get_formation_energy(chempots, cursor[0], energy_key=['temperature', 'free_energy_per_atom', '450'])
-        except KeyError:
-            errored = True
-        self.assertTrue(errored)
 
     def test_formation_energy_nonbinary(self):
         from matador.utils.chem_utils import get_formation_energy
@@ -260,9 +251,17 @@ class ChemUtilsTest(unittest.TestCase):
     def test_get_number_of_chempots(self):
         from matador.utils.chem_utils import get_number_of_chempots
 
+        stoich = [['V', 2]]
+        chempots = [[['Na', 1]], [['S', 1], ['O', 4]], [['V', 1]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [0, 0, 2])
+
         stoich = [['Li', 7], ['La', 3], ['Zr', 2], ['O', 12]]
         chempots = [[['Li', 2], ['O', 1]], [['Zr', 1], ['O', 2]], [['La', 2], ['O', 3]]]
         self.assertEqual(get_number_of_chempots(stoich, chempots), [3.5, 2, 1.5])
+
+        stoich = [['Li', 7], ['La', 3], ['Zr', 2], ['O', 12]]
+        chempots = [[['Li', 2], ['O', 1]], [['Zr', 1], ['O', 2]], [['La', 2], ['O', 3]]]
+        np.testing.assert_array_almost_equal(get_number_of_chempots(stoich, chempots, precision=None), [3.5, 2, 1.5])
 
         stoich = [['Li', 8], ['Zr', 3], ['O', 10]]
         chempots = [[['Li', 2], ['O', 1]], [['Zr', 1], ['O', 2]], [['La', 2], ['O', 3]]]
@@ -282,12 +281,37 @@ class ChemUtilsTest(unittest.TestCase):
 
         stoich = [['Li', 1], ['P', 1], ['O', 3]]
         chempots = [[['Li', 2], ['S', 1]], [['P', 2], ['O', 5]], [['P', 2], ['S', 5]]]
-        errored = False
-        try:
+        with self.assertRaises(RuntimeError):
             get_number_of_chempots(stoich, chempots)
-        except RuntimeError:
-            errored = True
-        self.assertTrue(errored)
+
+        stoich = [['Na', 1], ['B', 1], ['S', 1], ['O', 4]]
+        chempots = [[['Na', 1]], [['B', 1]], [['S', 1], ['O', 4]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [1, 1, 1])
+
+        stoich = [['Na', 1]]
+        chempots = [[['Na', 1]], [['B', 1]], [['S', 1], ['O', 4]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [1, 0, 0])
+
+        stoich = [['B', 1]]
+        chempots = [[['Na', 1]], [['B', 1]], [['S', 1], ['O', 4]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [0, 1, 0])
+
+        stoich = [['S', 2], ['O', 8]]
+        chempots = [[['Na', 1]], [['B', 1]], [['S', 1], ['O', 4]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [0, 0, 2])
+
+        stoich = [['B', 2], ['Na', 9]]
+        chempots = [[['Na', 1]], [['S', 1], ['O', 4]], [['B', 1]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [9, 0, 2])
+
+        stoich = [['B', 2], ['Na', 9]]
+        chempots = [[['Na', 1]], [['S', 1], ['O', 4]], [['B', 2]]]
+        self.assertEqual(get_number_of_chempots(stoich, chempots), [9, 0, 1])
+
+        stoich = [['A', 1], ['B', 2]]
+        chempots = [[['A', 1], ['B', 2], ['C', 3]]]
+        with self.assertRaises(RuntimeError):
+            get_number_of_chempots(stoich, chempots)
 
     def test_ratios_from_stoich(self):
         stoich = [['Li', 12], ['N', 18], ['P', 1]]
@@ -326,12 +350,8 @@ class ChemUtilsTest(unittest.TestCase):
         self.assertEqual(src, get_root_source(source))
 
         source = ['KP.cell', 'KP.param', 'abcd-123.fdasf/efgf/KP-0.02.-1234-abcd.castep', 'KP-1234-abcde.res']
-        failed = False
-        try:
+        with self.assertRaises(RuntimeError):
             src = get_root_source(source)
-        except RuntimeError:
-            failed = True
-        self.assertTrue(failed)
 
 
 if __name__ == '__main__':

matador/utils/chem_utils.py
@@ -26,7 +26,7 @@ ANGSTROM_CUBED_TO_CENTIMETRE_CUBED = 1e-24
 ELECTRON_CHARGE = physical_constants['elementary charge'][0]
 KELVIN_TO_EV = physical_constants['kelvin-electron volt relationship'][0]
 
-EPS = 1e-12
+EPS = 1e-8
 
 
 def get_iupac_ordering():
@@ -257,15 +257,21 @@ def get_formation_energy(chempots, doc, energy_key='enthalpy_per_atom'):
     return formation
 
 
-def get_number_of_chempots(stoich, chempot_stoichs):
+def get_number_of_chempots(stoich, chempot_stoichs, precision=5):
     """ Return the required number of each (arbitrary) chemical potentials
-    to construct one formula unit of the input stoichiometry.
+    to construct one formula unit of the input stoichiometry. Uses least-squares
+    as implemented by `numpy.linalg.lstsq` and rounds the output precision based
+    on the `precision` kwarg.
 
     Parameters:
         stoich (list/dict): matador-style stoichiometry,
             e.g. [['Li', 3], ['P', 1]], or the full document.
         chempot_stoichs (list/dict): list of stoichiometries of the input
             chemical potentials, or the full documents.
+
+    Keyword arguments:
+        precision (int/None): number of decimal places to round answer to. None
+            maintains the precision from `numpy.linalg.lstsq`.
 
     Returns:
         list: number of each chemical potential required to create
@@ -291,7 +297,6 @@ def get_number_of_chempots(stoich, chempot_stoichs):
     elements = sorted(list(elements))
 
     chempot_matrix = np.asarray([get_padded_composition(mu, elements) for mu in chempot_stoichs])
-    num_extraneous_equations = max(np.shape(chempot_matrix)) - min(np.shape(chempot_matrix))
 
     try:
         solution = np.asarray(get_padded_composition(stoich, elements))
@@ -299,21 +304,28 @@ def get_number_of_chempots(stoich, chempot_stoichs):
         raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: missing chempot'
                            .format(stoich, chempot_stoichs))
 
-    if num_extraneous_equations == 0:
-        num_chempots = scipy.linalg.solve(chempot_matrix.T, solution)
-    else:
-        num_chempots = scipy.linalg.solve(chempot_matrix[:, :-num_extraneous_equations].T, solution[:-num_extraneous_equations])
-        # clean near zero float values
+    try:
+        num_chempots, residuals, _, _ = np.linalg.lstsq(chempot_matrix.T, solution, rcond=None)
+    except np.linalg.LinAlgError:
+        raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: numpy LinAlg error'
+                           .format(stoich, chempot_stoichs))
+
+    # check if lstsq actually found a "solution"
+    if np.abs(np.sum(residuals)) > EPS:
+        raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: inconsistent chemical potentials'
+                           .format(stoich, chempot_stoichs))
+
+    # round output array based on user-specified precision
+    if precision is not None:
         num_chempots[np.where(np.abs(num_chempots) < EPS)] = 0.0
-        if np.min(np.sign(num_chempots)) == -1:
-            raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: stoichiometry inconsistent with chempots'
-                               .format(stoich, chempot_stoichs))
-        # check equations are consistent
-        for i in range(1, num_extraneous_equations+1):
-            verify = sum(num_chempots * chempot_matrix[:, -i])
-            if np.abs(verify - solution[-i]) > EPS:
-                raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: stoichiometry inconsistent with chempots'
-                                   .format(stoich, chempot_stoichs))
+        for i, val in enumerate(num_chempots):
+            if np.abs(val - round(val, precision)) < EPS:
+                num_chempots[i] = round(val, precision)
+
+    # check for sensible numbers in output
+    if np.min(np.sign(num_chempots)) == -1:
+        raise RuntimeError('Stoichiometry {} could not be created from chemical potentials {}: stoichiometry inconsistent with chempots'
+                           .format(stoich, chempot_stoichs))
 
     return num_chempots.tolist()
 