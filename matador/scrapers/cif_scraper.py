FIX: generalised cif scraper to work for any cell shape at any numerical precision

matador/scrapers/cif_scraper.py
@@ -9,7 +9,7 @@
 import numpy as np
 from matador.scrapers.utils import scraper_function
 from matador.utils.cell_utils import get_spacegroup_spg
-from matador.utils.cell_utils import abc2cart, cart2volume
+from matador.utils.cell_utils import abc2cart, cart2volume, frac2cart
 
 EPS = 1e-13
 
@@ -207,6 +207,9 @@ def _cif_set_unreduced_sites(doc):
 
     """
     from matador.utils.cell_utils import wrap_frac_coords
+    from matador.utils.cell_utils import calc_pairwise_distances_pbc
+    from matador.fingerprints.pdf import PDF
+    from collections import defaultdict
     species_sites = dict()
     species_occ = dict()
     for ind, site in enumerate(doc['positions_frac']):
@@ -235,6 +238,8 @@ def _cif_set_unreduced_sites(doc):
     unreduced_sites = []
     unreduced_occupancies = []
     unreduced_species = []
+
+    # this loop assumes that no symmetry operation can map 2 unlike sites upon one another
     for species in species_sites:
         unreduced_sites_spec, indices = np.unique(np.around(species_sites[species], decimals=5),
                                                   return_index=True, axis=0)
@@ -243,9 +248,37 @@ def _cif_set_unreduced_sites(doc):
         unreduced_sites.extend(unreduced_sites_spec.tolist())
         unreduced_species.extend(len(unreduced_sites_spec) * [species])
 
+    images = PDF._get_image_trans_vectors_auto(doc['lattice_cart'], 0.1, 0.01, max_num_images=2)
+    poscarts = frac2cart(doc['lattice_cart'], unreduced_sites)
+    distances = calc_pairwise_distances_pbc(
+        poscarts,
+        images,
+        doc['lattice_cart'],
+        0.1,
+        compress=False,
+        per_image=True
+    )
+
+    dupe_dict = defaultdict(list)
+    dupe_set = set()
+    for img in distances:
+        for i in range(len(poscarts)):
+            for j in range(len(poscarts)):
+                if i == j:
+                    continue
+                if not img.mask[i, j]:
+                    if i not in dupe_set and unreduced_occupancies[i] == 1:
+                        dupe_dict[i].append(j)
+                        dupe_set.add(j)
+
     doc['positions_frac'] = unreduced_sites
     doc['site_occupancy'] = unreduced_occupancies
     doc['atom_types'] = unreduced_species
+
+    doc['site_occupancy'] = [doc['site_occupancy'][ind] for ind, atom in enumerate(doc['positions_frac']) if ind not in dupe_set]
+    doc['atom_types'] = [doc['atom_types'][ind] for ind, atom in enumerate(doc['positions_frac']) if ind not in dupe_set]
+    doc['positions_frac'] = [atom for ind, atom in enumerate(doc['positions_frac']) if ind not in dupe_set]
+
     tmp = sum(doc['site_occupancy'])
     if abs(tmp - round(tmp, 0)) < EPS:
         tmp = round(tmp, 0)

matador/utils/cell_utils.py
@@ -511,7 +511,7 @@ def add_noise(doc, amplitude=0.1):
 
 def calc_pairwise_distances_pbc(poscart, images, lattice, rmax,
                                 poscart_b=None, compress=False, debug=False,
-                                filter_zero=False):
+                                filter_zero=False, per_image=False):
     """ Calculate PBC distances with SciPy's cdist, given the
     image cell vectors.
 
@@ -531,10 +531,13 @@ def calc_pairwise_distances_pbc(poscart, images, lattice, rmax,
             useful when e.g. creating PDFs but not when atom ID is important.
         filter_zero (bool): whether or not to filter out the "self-interaction"
             zero distances.
+        per_image (bool): return a list of distances per image, as opposed to
+            one large flat. This preserves atom IDs for use elsewhere.
 
     Returns:
         distances (numpy.ndarray): pairwise 2-D d_ij masked array with values
-            or stripped 1-D array containing just the distances.
+            or stripped 1-D array containing just the distances, or a list of
+            numpy arrays if per_image is True.
 
     """
     from scipy.spatial.distance import cdist
@@ -543,29 +546,38 @@ def calc_pairwise_distances_pbc(poscart, images, lattice, rmax,
         start = time.time()
     _lattice = np.asarray(lattice)
     _poscart = np.asarray(poscart)
+    image_distances = []
     if poscart_b is None:
         _poscart_b = _poscart
     else:
         _poscart_b = np.asarray(poscart_b)
 
-    distances = np.empty((len(_poscart)*len(images)*len(_poscart_b)))
     num_pairs = len(_poscart) * len(_poscart_b)
 
-    for image_ind, prod in enumerate(images):
-        distances[image_ind*num_pairs:(image_ind+1)*num_pairs] = cdist(_poscart, _poscart_b + prod @ _lattice).flatten()
+    if per_image:
+        for image_ind, prod in enumerate(images):
+            distances = cdist(_poscart, _poscart_b + prod @ _lattice)
+            distances = np.ma.masked_where(distances > rmax, distances, copy=False)
+            image_distances.append(distances)
 
-    # mask by rmax/0 and remove masked values
-    distances = np.ma.masked_where(distances > rmax, distances, copy=False)
-    if filter_zero:
-        distances = np.ma.masked_where(distances < EPS, distances, copy=False)
+        distances = image_distances
 
+    else:
+        array_size = (len(_poscart) * len(images) * len(_poscart_b))
+        distances = np.empty(array_size)
+        for image_ind, prod in enumerate(images):
+            distances[image_ind*num_pairs:(image_ind+1)*num_pairs] = cdist(_poscart, _poscart_b + prod @ _lattice).flatten()
 
-    if debug:
-        print('Calculated: {}, Used: {}, Ignored: {}'.format(len(distances),
-                                                             np.ma.count(distances),
-                                                             np.ma.count_masked(distances)))
-    if compress:
-        distances = distances.compressed()
+        distances = np.ma.masked_where(distances > rmax, distances, copy=False)
+        if filter_zero:
+            distances = np.ma.masked_where(distances < EPS, distances, copy=False)
+
+        if debug:
+            print('Calculated: {}, Used: {}, Ignored: {}'.format(len(distances),
+                                                                 np.ma.count(distances),
+                                                                 np.ma.count_masked(distances)))
+        if compress:
+            distances = distances.compressed()
 
     if debug:
         end = time.time()