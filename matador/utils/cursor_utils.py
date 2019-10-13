e152d1871d9e7d2f15288b4080312f80768b39a2
Matthew Evans <me388@cam.ac.uk>
2019-10-13 13:18:37 +0100

    MAINT: support for Pauling File/Materials Project when displaying cursors

--- matador/utils/cursor_utils.py
+++ matador/utils/cursor_utils.py
@@ -531,7 +531,7 @@ def get_guess_doc_provenance(sources, icsd=None):
     from its source list.
 
     Return possiblities are 'ICSD', 'SWAP', 'OQMD' or
-    'AIRSS'.
+    'AIRSS', 'MP' or 'PF'.
     """
     prov = 'AIRSS'
     if sources is str:
@@ -540,7 +540,9 @@ def get_guess_doc_provenance(sources, icsd=None):
         fname_with_folder = fname
         fname = fname.split('/')[-1].lower()
         if (fname.endswith('.castep') or fname.endswith('.res') or fname.endswith('.history') or
-                ('oqmd' in fname and fname.count('.') == 0)):
+                ('oqmd' in fname and fname.count('.') == 0) or
+                ('mp-' in fname and fname.count('.') == 0) or
+                ('pf-' in fname and fname.count('.') == 0)):
             if any(substr in fname for substr in ['collcode', 'colcode', 'collo']):
                 if fname.count('-') == 2 + fname.count('oqmd') or 'swap' in fname:
                     prov = 'SWAPS'
@@ -556,7 +558,11 @@ def get_guess_doc_provenance(sources, icsd=None):
                 prov = 'OQMD'
             elif '-icsd' in fname:
                 prov = 'ICSD'
-            elif '-mp-' in fname:
+            elif 'pf-' in fname:
+                prov = 'PF'
+            elif 'mp-' in fname:
+                if prov == 'PF':
+                    continue
                 prov = 'MP'
             elif '-sm-' in fname:
                 prov = 'SM'
