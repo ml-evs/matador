7a0c384e1b3deed683ea03d9664fcb48eb31004a
Matthew Evans <me388@cam.ac.uk>
2019-10-24 22:27:05 +0100

    MAINT: Removed debug print [skip ci]

--- matador/export/export.py
+++ matador/export/export.py
@@ -131,7 +131,6 @@ def query2files(cursor, **kwargs):
             root_source = root_source.replace('-swap-', '-')
         name = root_source
 
-        print(root_source, formula, name)
         if 'OQMD ' in root_source:
             name = '{formula}-OQMD_{src}'.format(formula=formula, src=root_source.split(' ')[-1])
         elif 'mp-' in root_source:
