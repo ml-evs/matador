7f030b95f594982d55bfdad8d45f4c0320fcc1c0
Matthew Evans <me388@cam.ac.uk>
2020-01-31 14:31:03 +0000

    ENH: improved temperature plotting functionality

--- matador/plotting/temperature_plotting.py
+++ matador/plotting/temperature_plotting.py
@@ -11,6 +11,45 @@ from matador.plotting.plotting import plotting_function
 
 
 @plotting_function
+def plot_free_energy(model, temperatures=None, ax=None, line_kwargs=None, **kwargs):
+    """ Plot G(T) on the array of given temperatures. Default T is [0, 800].
+
+    Keyword arguments:
+        temperatures (list/np.ndarray): list or array of temperatures to plot.
+            If the array/list has length 2, use these as the start and endpoints
+            with 21 plotting points.
+        ax (matplotlib.pyplot.Axis): axis object to plot onto.
+
+    """
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    if line_kwargs is None:
+        line_kwargs = {}
+
+    num_points = 21
+    start = 0
+    end = 800
+    if temperatures is not None and len(temperatures) == 2:
+        start = temperatures[0]
+        end = temperatures[1]
+
+    if temperatures is None or len(temperatures) == 2:
+        _temperatures = np.linspace(start, end, num_points)
+    elif temperatures is not None:
+        _temperatures = temperatures
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    t, e = model.vibrational_free_energy(temperatures=_temperatures)
+    ax.plot(t, e, **line_kwargs)
+    ax.set_ylabel("Vibrational free energy")
+    ax.set_xlabel("Temperature (K)")
+
+
+@plotting_function
 def plot_thermo_curves(seed, show=False, **kwargs):
     """ Plot free energies and heat capacity as a function of temperature, as
     calculated from a CASTEP thermodynamics run in <seed>.castep.
