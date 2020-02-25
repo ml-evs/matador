a5536888ff642a14f87d785d87a71fce68716fd2
Matthew Evans <me388@cam.ac.uk>
2020-02-25 17:23:46 +0000

    Refactored IR plotting to use phonon2dict

--- matador/plotting/ir_plotting.py
+++ matador/plotting/ir_plotting.py
@@ -3,122 +3,88 @@ the results of infrared and Raman spectroscopy calculations.
 
 """
 
+import numpy as np
+from matador.scrapers import phonon2dict
 from matador.plotting.plotting import plotting_function
+from matador.utils.chem_utils import INVERSE_CM_TO_EV
 
 
-def read_ir_file(seed):
-    """ This function should take the seed filename and
-    read in the IR data, probably into a dictionary.
+@plotting_function
+def plot_ir_spectrum(seed, bin_width=1.0, ax=None, show=True, **kwargs):
+    """ This function plots the IR/Raman spectrum found in the given .phonon file.
 
     Parameters:
-        seed (str): the filename to read.
+        seed (str): the filename to scrape and plot.
+
+    Keyword arguments:
+        bin_width (float): the bin width for the IR plot.
+        ax (matplotlib.axes.Axes): an existing axis on which to plot.
+        show (bool): whether or not to display the plot in an X window
+
+    Raises:
+        RuntimeError: if unable to scrape IR data.
 
     Returns:
-        dict: containing IR data
+        matplotlib.axes.Axes: matplotlib axis with plotted data.
 
     """
 
-    if seed.endswith('phonon'):
-        seed = seed.replace('.phonon', '')
-    with open(seed + '.phonon', 'r') as f:
-        flines = f.readlines()
-    for ind, line in enumerate(flines):
-        if 'Number of branches' in line:
-            no_branches = int(line.split()[3])
-        if 'END header' in line:
-            header_end = ind
-
-    # contains IR and Raman data
-    ir_dat = dict()
-    for line in flines[header_end+2:header_end+no_branches+2]:
-        line_split = line.split()
-        if len(line_split) < 2:
-            raise RuntimeError ('Failed to read IR data. Check .phonon file.')
-        wavenumber = float(line_split[1])
-        ir = float(line_split[2])
-        # checks for raman data
-        if len(line_split) > 3:
-            raman = float(line_split[3])
-            if wavenumber in ir_dat:
-                ir_dat[wavenumber] = [ir_dat[wavenumber][0]+ir, ir_dat[wavenumber][1]+raman]
-            else:
-                ir_dat[wavenumber] = [ir, raman]
-        else:
-            if wavenumber in ir_dat:
-                ir_dat[wavenumber] = [ir_dat[wavenumber][0]+ir]
-            else:
-                ir_dat[wavenumber] = [ir]
-
-    return ir_dat
-
-# this decorator will automatically set the style of the plot
-# to match the user's config/matador defaults, and safely handles
-# e.g. X-forwarding and file writing
-@plotting_function
-def plot_ir_spectrum(seed, step_size):
-    """ This function plots the IR spectrum found in the given file.
-    Parameters:
-        bin_width(float): The width of the wavenumber bin in cm^-1. Determined by step_size and bin_scaler
-
-    Keyword Arguments:
-        step_size(float): change in wavenumber between points on x-axis,
-    """
     import matplotlib.pyplot as plt
 
-    # by default, the bin width is the same size as the step size but can be modified if needed
-    bin_width = step_size * 1
-
-    # read IR output
-    ir_dat = read_ir_file(seed)
-    wavenumbers_castep = list(ir_dat.keys())
-    if len(ir_dat[wavenumbers_castep[0]]) == 2:
-        raman = True
+    if ax is None:
+        fig, ax_ir = plt.subplots()
     else:
-        raman = False
-    min_wavenumber = int(wavenumbers_castep[0])-(2*step_size)
-    max_wavenumber = int(wavenumbers_castep[len(wavenumbers_castep)-1])+(2*step_size)
-    wavenumbers_plot = []
-    for i in range(min_wavenumber, max_wavenumber, step_size):
-        wavenumbers_plot.append(i)
+        ax_ir = ax
 
-    ir_plot = []
-    raman_plot = []
-    for i in wavenumbers_plot:
-        ir_intensity = 0
-        raman_intensity = 0
-        for j in wavenumbers_castep:
-            if j >= (i-(0.5*bin_width)) and j < (i+(0.5*bin_width)):
-                ir_intensity = ir_intensity + ir_dat[j][0]
-                if raman:
-                    raman_intensity = raman_intensity + ir_dat[j][1]
-                pass
-        ir_plot.append(ir_intensity)
-        if raman:
-            raman_plot.append(raman_intensity)
+    ir_data, s = phonon2dict(seed)
+    if not s or 'infrared_intensity' not in ir_data:
+        raise RuntimeError('Error scraping file: no IR intensities. {}'.format(ir_data))
 
-    ir_max = 0
-    raman_max = 0
-    for i in wavenumbers_castep:
-        if ir_dat[i][0] > ir_max:
-            ir_max = ir_dat[i][0]
-        if raman:
-            if ir_dat[i][1] > raman_max:
-                raman_max = ir_dat[i][1]
+    wavenumbers = ir_data['eigenvalues_q'] / INVERSE_CM_TO_EV
 
-    fig, ax1 = plt.subplots(figsize = (14,8))
-    plt.title(seed)
-    ax1.plot(wavenumbers_plot, ir_plot, color='#EE3425')
-    ax1.set_xlabel('Wavenumbers (cm$^{-1}$)')
-    ax1.set_ylabel('IR intensities  ((D/A)**2/amu)', color='#EE3425')
+    plotting_raman = 'raman_intensity' in ir_data
+
+    max_wavenumber = np.max(wavenumbers)
+    bins = np.linspace(0, 1.2 * max_wavenumber, num=int((1 / bin_width) * 1.2 * max_wavenumber))
+
+    ir_spectrum, bin_edges = np.histogram(
+        wavenumbers,
+        bins=bins,
+        weights=ir_data['infrared_intensity'],
+        density=True
+    )
+    # normalize so the highest peak is at 1
+    ir_spectrum /= np.max(ir_spectrum)
+    if plotting_raman:
+        raman_spectrum, _ = np.histogram(
+            wavenumbers,
+            bins=bins,
+            weights=ir_data['raman_intensity'],
+            density=True
+        )
+        raman_spectrum /= np.max(raman_spectrum)
+
+    # get bin centres
+    bins = 0.5 * (bin_edges[1:] + bin_edges[:-1])
+
+    ax_ir.plot(bins, ir_spectrum, color='#EE3425')
+    ax_ir.set_xlabel('Wavenumbers (cm$^{-1}$)')
+    ax_ir.set_ylabel('Relative IR intensities', color='#EE3425')
     plt.gca().invert_yaxis()
     plt.gca().invert_xaxis()
+    ir_max = np.max(ir_spectrum)
+    ax_ir.set_ylim(2.1 * ir_max, -ir_max * 0.05)
+    ax_ir.set_yticks(np.linspace(0, 1, 6))
 
-    if raman:
-        ax1.set_ylim(2.1*ir_max,-(ir_max*0.05))
-        ax2 = ax1.twinx()
-        ax2.plot(wavenumbers_plot, raman_plot, color='#236DE8')
-        ax2.set_ylabel('Raman activies (A**4 amu**(-1))', color='#236DE8')
-        ax2.set_ylim(-(raman_max*0.05), 2.1*raman_max)
+    if plotting_raman:
+        ax_raman = ax_ir.twinx()
+        raman_max = np.max(raman_spectrum)
+        ax_raman.set_yticks(np.linspace(0, 1, 6))
+        ax_raman.set_ylim(-raman_max * 0.05, 2.1 * raman_max)
+        ax_raman.plot(bins, raman_spectrum, color='#236DE8')
+        ax_raman.set_ylabel('Relative Raman activity', color='#236DE8')
+
+    plt.title(seed)
 
     plt.show()
     #return ax
