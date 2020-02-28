efddb625f69e37eb1bb799ca6de8fb670e90d19c
Matthew Evans <me388@cam.ac.uk>
2020-02-28 00:14:59 +0000

    MAINT: only write the required pspots in cell file, not all present

--- matador/export/export.py
+++ matador/export/export.py
@@ -477,7 +477,7 @@ def doc2cell(doc, path, *args, **kwargs):
         for elem in doc['species_pot']:
             if elem == 'library':
                 flines.append(doc['species_pot']['library'])
-            else:
+            elif elem in doc['atom_types']:
                 flines.append('{:4} {}'.format(elem, doc['species_pot'][elem]))
         flines.append('%ENDBLOCK SPECIES_POT')
 
