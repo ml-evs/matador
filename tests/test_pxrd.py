5a7065f6eb50fae623089e18c115b21484d27e6d
Matthew Evans <me388@cam.ac.uk>
2020-05-01 19:48:15 +0100

    TST: added many PXRD tests to compare against GSASII

--- tests/test_pxrd.py
+++ tests/test_pxrd.py
@@ -7,49 +7,57 @@ from os.path import realpath
 
 REAL_PATH = "/".join(realpath(__file__).split("/")[:-1]) + "/"
 DEBUG = True
-PEAK_POS_TOL = 2
+PEAK_POS_TOL = 0.02
 
 
-class PDFCalculatorTest(unittest.TestCase):
-    """ Test PDF calculator. """
+def _match_peaks(res_file, gsas_reflections, **kwargs):
+    """ Check if peaks match with HKL between matador and GSAS. """
+    doc, s = res2dict(res_file, as_model=True)
+
+    pxrd = PXRD(doc, **kwargs)
+    gsas_peaks = np.loadtxt(gsas_reflections)
+    hkl_array = {}
+    for peak in gsas_peaks:
+        hkl_array[(int(peak[0]), int(peak[1]), int(peak[2]))] = peak[3]
+
+    for ind, hkl in enumerate(pxrd.hkls):
+        hkl_tuple = (int(hkl[0]), int(hkl[1]), int(hkl[2]))
+        if hkl_tuple in hkl_array:
+            assert (
+                np.abs(hkl_array[hkl_tuple] - pxrd.peak_positions[ind]) < PEAK_POS_TOL
+            )
+
+    return pxrd
+
+
+class PXRDCalculatorTest(unittest.TestCase):
+    """ Test PXRD calculator. """
 
     def test_simple_pxrd(self):
-        doc, success = res2dict(REAL_PATH + "data/structures/Li.res")
-        doc["pxrd-Cu"] = PXRD(
-            doc, two_theta_resolution=0.0001, gaussian_width=0.001, plot=False, wavelength=1.5406,
+        """ Test Li PXRD vs GSAS. """
+        pxrd = _match_peaks(
+            REAL_PATH + "data/structures/Li.res",
+            REAL_PATH + "data/pxrd_files/Li_reflections_Cu.txt",
         )
-        peaks = doc["pxrd-Cu"].peak_positions
-        # peaks grabbed from Materials Project for Cu K-alpha
-        Li_peaks = [
-            36.01,
-            41.822,
-            60.63,
-            72.581,
-            76.369,
-            91.095,
-            102.134,
-            105.896,
-            121.916,
-            136.034,
-        ]
-        for peak in Li_peaks:
-            # dodgy test for now: check every peak is present to within 1 degrees
-            self.assertTrue(
-                len(peaks[np.abs(peaks - peak) < 1]) > 2,
-                msg="Missing peak at {}".format(peak),
-            )
-        # peaks grabbed from Materials Project for Ag K-alpha
-        doc["pxrd-Ag"] = PXRD(
-            doc, two_theta_resolution=0.0001, gaussian_width=0.001, wavelength=0.56
+        self.assertAlmostEqual(
+            pxrd.two_thetas[np.argmax(pxrd.pattern)], 30.969, places=2
         )
-        peaks = doc["pxrd-Ag"].peak_positions
-        Li_peaks = [12.912, 14.92, 21.161, 24.868, 25.992, 30.102, 32.876]
-        print(np.sort(peaks))
-        for peak in Li_peaks:
-            # dodgy test for now: check every peak is present to within 1 degree
-            self.assertTrue(
-                len(peaks[np.abs(peaks - peak) < 1]) > 2,
-                msg="Missing peak at {}".format(peak),
-            )
+        pxrd_args = {"wavelength": 0.559363}
+        pxrd = _match_peaks(
+            REAL_PATH + "data/structures/Li.res",
+            REAL_PATH + "data/pxrd_files/Li_reflections_Ag.txt",
+            **pxrd_args
+        )
+        self.assertAlmostEqual(
+            pxrd.two_thetas[np.argmax(pxrd.pattern)], 30.969, places=2
+        )
 
-        # TODO: need to check amplitudes too and factory
+    def test_CuP2_vs_GSAS(self):
+        """ Test CuP2 peak positions vs GSAS. """
+        pxrd = _match_peaks(
+            REAL_PATH + "data/pxrd_files/CuP2.res",
+            REAL_PATH + "data/pxrd_files/CuP2_GSASII_reflections.txt",
+        )
+        self.assertAlmostEqual(
+            pxrd.two_thetas[np.argmax(pxrd.pattern)], 30.969, places=2
+        )
