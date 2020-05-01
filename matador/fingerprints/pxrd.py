e6717495cb4e88a9ab2cca76b78bd95b9c39eb23
Matthew Evans <me388@cam.ac.uk>
2020-05-01 19:49:26 +0100

    ENH: complete rewrite of PXRD, 10-20x speedup and improved accuracy

--- matador/fingerprints/pxrd.py
+++ matador/fingerprints/pxrd.py
@@ -1,17 +1,18 @@
 # coding: utf-8
 # Distributed under the terms of the MIT License.
 
-""" This file implements the PXRD class for simulating powder XRD spectrum
+""" This file implements the PXRD class for simulating powder XRD pattern
 of a crystal.
 
 """
 
-import copy
-import os
+import itertools
+
 import numpy as np
 
 from matador.fingerprints.fingerprint import Fingerprint, FingerprintFactory
-from matador.utils.cell_utils import frac2cart, standardize_doc_cell
+from matador.crystal import Crystal
+from matador.utils.cell_utils import standardize_doc_cell, real2recip
 from matador.utils.chem_utils import get_formula_from_stoich
 
 THETA_TOL = 1e-5
@@ -22,18 +23,26 @@ class PXRD(Fingerprint):
     given crystal for a certain incident wavelength. The cell is
     standardised with spglib before computing PXRD.
 
+    This calculation takes into account atomic scattering factors, Lorentz
+    polarisation and thermal broadening (with Debye-Waller factors set to
+    1). Note: this class does not perform any q-dependent peak broadening,
+    and instead uses a simple Lorentzian broadening. The default width
+    of 0.03 provides good agreement with e.g. GSAS-II's default CuKa setup.
+    Only one wavelength can be used at a time, but multiple patterns could be
+    combined post hoc.
+
     Attributes:
-        self.peak_positions (numpy.ndarray): peak positions as values
-            in 2θ
-        self.spectrum (numpy.ndarray): gaussian-broadened spectrum at
+        self.peak_positions (numpy.ndarray): sorted peak positions as
+            values in 2θ
+        self.hkls (numpy.ndarray): Miller indices correspnding to peaks,
+            sorted by peak angle.
+        self.peak_intensities (numpy.ndarray): intensity of each peak.
+        self.pattern (numpy.ndarray): Lorentzian-broadened pattern at
             values of self.two_thetas.
         self.two_thetas (numpy.ndarray): continuous space of 2θ values
-            corresponding to sample points of self.spectrum.
+            corresponding to sample points of self.pattern.
 
     """
-    # TODO: group peaks and return amplitudes and positions
-    # TODO: vectorise loop over q-vectors
-    # TODO: check atomic scattering data
     def __init__(self, doc, lazy=False, plot=False, *args, **kwargs):
         """ Set up the PXRD, and compute it, if lazy is False.
 
@@ -41,44 +50,56 @@ class PXRD(Fingerprint):
             doc (dict): matador document to compute PXRD for.
 
         Keyword arguments:
-            lazy (bool): whether to compute PXRD or just set it up.
-            plot (bool): whether to display PXRD as a plot.
-            gaussian_width (float): width of gaussians for broadening.
+            lorentzian_width (float): width of Lorentzians for broadening (DEFAULT: 0.03)
             wavelength (float): incident X-ray wavelength
+                (DEFAULT: CuKa, 1.5406).
             two_theta_resolution (float): resolution of grid 2θ
                 used for plotting.
+            two_theta_bounds (tuple of float): values between which
+                to compute the PXRD pattern.
+            scattering_factors (str): either "GSAS" or "RASPA" (default),
+                which set of atomic scattering factors to use.
+            lazy (bool): whether to compute PXRD or just set it up.
+            plot (bool): whether to display PXRD as a plot.
 
         """
-        prop_defaults = {'wavelength': 1.5406,
-                         'gaussian_width': 0.1,
-                         'two_theta_resolution': 0.001}
+        prop_defaults = {
+            'wavelength': 1.5406,
+            'lorentzian_width': 0.03,
+            'two_theta_resolution': 0.01,
+            'two_theta_bounds': [0, 90],
+            'scattering_factors': "RASPA",
+        }
 
-        options = copy.deepcopy(prop_defaults)
+        options = dict(prop_defaults)
         options.update(kwargs)
         self.wavelength = options['wavelength']
-        self.gaussian_width = options['gaussian_width']
+        self.lorentzian_width = options['lorentzian_width']
         self.two_theta_resolution = options['two_theta_resolution']
+        self.two_theta_bounds = list(options['two_theta_bounds'])
 
-        self.doc = standardize_doc_cell(doc)
+        if self.two_theta_bounds[0] < THETA_TOL:
+            self.two_theta_bounds[0] = THETA_TOL
+
+        self.doc = Crystal(standardize_doc_cell(doc, primitive=True))
         self.formula = get_formula_from_stoich(self.doc['stoichiometry'], tex=True)
         self.spg = self.doc['space_group']
 
-        species = list({species for species in self.doc['atom_types']})
+        species = list(set(self.doc['atom_types']))
+        self.options = options
 
-        self.atomic_scattering_coeffs = {}
-        data_file = os.path.dirname(os.path.realpath(__file__)) + '/../data/atomic_scattering_factors.dat'
-        with open(data_file, 'r') as f:
-            flines = f.readlines()
-        for line in flines:
-            x = line.split()
-            label = x[0]
-            if label in species:
-                a_inds = [1, 3, 5, 7]
-                b_inds = [2, 4, 6, 8]
-                a = np.array([float(x[ind]) for ind in a_inds])
-                b = np.array([float(x[ind]) for ind in b_inds])
-                c = float(x[9])
-                self.atomic_scattering_coeffs[label] = [a, b, c]
+        # this could be cached across PXRD objects but is much faster than the XRD calculation itself
+        if options.get('scattering_factors') == "GSAS":
+            from matador.data import GSAS_ATOMIC_SCATTERING_COEFFS
+            self.atomic_scattering_coeffs = {spec: GSAS_ATOMIC_SCATTERING_COEFFS[spec] for spec in species}
+        elif options.get('scattering_factors') == "RASPA":
+            from matador.data import RASPA_ATOMIC_SCATTERING_COEFFS
+            self.atomic_scattering_coeffs = {spec: RASPA_ATOMIC_SCATTERING_COEFFS[spec] for spec in species}
+        else:
+            raise RuntimeError(
+                "No set of scattering factors matched: {}. Please use 'GSAS' or 'RASPA'."
+                .format(options.get('scattering_factors'))
+            )
 
         if not lazy:
             self.calculate()
@@ -86,78 +107,110 @@ class PXRD(Fingerprint):
                 self.plot()
 
     def calc_pxrd(self):
-        two_theta_bounds = [10, 140]
-        lattice_abc = np.array(self.doc['lattice_abc'])
-        self.lattice_cart = lattice_cart = np.array(self.doc['lattice_cart'])
+        """ Calculate the PXRD pattern. """
+
+        # set crystallographic data
+        lattice_abc = np.asarray(self.doc.lattice_abc)
+        lattice_cart = np.asarray(self.doc.lattice_cart)
+        positions_abs = np.asarray(self.doc.positions_abs)
 
         # find allowed reciprocal lattice points within limiting sphere
-        Ns = np.floor((2 / self.wavelength) * lattice_abc[0, :] *
-                      np.sin((two_theta_bounds[1] / 2) * np.pi / 180)).astype(int)
+        min_r, max_r = [2 / self.wavelength * np.sin(np.pi / 180 * t / 2) for t in self.two_theta_bounds]
+        Ns = np.floor(max_r * lattice_abc[0, :]).astype(int)
 
-        R_inv = np.linalg.inv(lattice_cart)
-        qs = []
-        for i in range(-Ns[0], Ns[0]+1):
-            for j in range(-Ns[1], Ns[1]+1):
-                for k in range(-Ns[2], Ns[2]+1):
-                    qs.append(np.dot(R_inv, 2*np.pi * np.array([i, j, k])))
-        qs = np.asarray(qs)
+        recip = np.asarray(real2recip(lattice_cart)).T
+        qs = np.zeros((2*sum(Ns), 3), dtype=np.float64)
+        hkls = np.asarray(
+            list(itertools.product(
+                range(-Ns[0], Ns[0] + 1),
+                range(-Ns[1], Ns[1] + 1),
+                range(-Ns[2], Ns[2] + 1)
+            )), dtype=np.float64
+        )
+        hkls = hkls[np.argsort(np.linalg.norm(hkls, axis=-1))]
+        qs = np.dot(recip, hkls.T).T
+
+        # filter out by theta bounds
+        q_mags = np.linalg.norm(qs, axis=-1)
+        allowed = np.where(np.logical_and(q_mags <= max_r * 2 * np.pi, q_mags >= min_r * 2 * np.pi))
+        qs = qs[allowed]
+        hkls = hkls[allowed]
 
         # compute Bragg condition to find peak locations
         sin_tau = np.linalg.norm(qs, axis=1) * self.wavelength / (4 * np.pi)
         sin_tau[sin_tau > 1] = 0
         taus = 2 * np.arcsin(sin_tau)
-        qs = qs[np.where(taus > THETA_TOL)]
-        taus = taus[taus > THETA_TOL]
 
         # compute structure factor S(q) as sum of atomic scattering factors
         S_q = np.zeros_like(taus)
+
         for ind, q_vector in enumerate(qs):
-            S_q[ind] = np.abs(self.structure_factor(q_vector))**2
+            # accumulate atomic scattering factors
+            atomic_factor = {}
+            for species in set(self.doc['atom_types']):
+                atomic_factor[species] = self.atomic_scattering_factor(q_mags[ind], species)
+            factors = np.array([atomic_factor[species] for species in self.doc['atom_types']])
+            F_s = np.sum(np.exp(1j * positions_abs @ q_vector) * factors)
+            S_q[ind] = np.abs(F_s)**2
 
         # apply Lorentz correction for polarisation and finite size effects
-        S_q *= 0.5 * (1 + np.cos(taus) ** 2) / (np.sin(taus) * np.sin(0.5 * taus))
+        S_q *= 2 * (1 + np.cos(taus) ** 2) / (np.sin(taus) * np.sin(0.5 * taus))
 
-        # thermal correction: TODO: what is this correction? surely depends on Debye-Waller
-        B = 1
-        S_q *= np.exp(-B * np.sin(taus)**2 / self.wavelength**2)**2
+        # thermal correction assuming no Debye-Waller factor
+        S_q *= np.exp(-np.sin(taus)**2 / self.wavelength**2)**2
+        S_q /= np.max(S_q)
 
         # create histogram and broaden onto 2 theta space in degrees
         self.peak_positions = (180 / np.pi) * taus
-        self.two_thetas = np.arange(two_theta_bounds[0],
-                                    two_theta_bounds[1] + self.two_theta_resolution,
+        self.hkls = hkls
+        self.two_thetas = np.arange(self.two_theta_bounds[0],
+                                    self.two_theta_bounds[1] + self.two_theta_resolution,
                                     self.two_theta_resolution)
-        self.spectrum, bins = np.histogram(self.peak_positions, bins=self.two_thetas, weights=S_q)
-        if self.gaussian_width > 0:
-            self.spectrum = self._broadening_unrolled(self.spectrum, self.two_thetas, self.gaussian_width)
-        self.spectrum /= np.max(self.spectrum)
+
+        self.pattern, bins = np.histogram(self.peak_positions, bins=self.two_thetas, weights=S_q)
+
+        if self.lorentzian_width > 0:
+            self.pattern = self._broadening_unrolled(
+                self.pattern, self.two_thetas, self.lorentzian_width, broadening_type='lorentzian'
+            )
+        else:
+            # shift and clip the last two theta value if we didnt do broadening
+            self.two_thetas = self.two_thetas[:-1] + self.two_theta_resolution / 2
+
+        self.pattern /= np.max(self.pattern)
+
+        order = np.argsort(self.peak_positions)
+        self.hkls = hkls[order]
+        self.peak_intensities = S_q
+        self.peak_positions = self.peak_positions[order]
+
+        # alias old name for compatibility...
+        self.spectrum = self.pattern
 
     def calculate(self):
+        """ Alias for calculating the PXRD pattern. """
         self.calc_pxrd()
 
-    def structure_factor(self, q_vector):
-        F_s = 0
-        q_mag = np.linalg.norm(q_vector)
-        for ind, species in enumerate(self.doc['atom_types']):
-            phase = np.dot(q_vector, frac2cart(self.lattice_cart, self.doc['positions_frac'][ind]))
-            atomic_factor = self.atomic_scattering_factor(q_mag, species)
-            F_s += np.exp(1j * phase) * atomic_factor
-
-        return F_s
-
     def atomic_scattering_factor(self, q_mag, species):
-        # return fit for particular atom at given q
+        """ Return fit for particular atom at given q-vector.
+
+        Parameters:
+            q_mag (float): magnitude of the q_vector.
+            species (str): the element label.
+
+        Returns:
+            float: the atomic scattering factor.
+
+        """
         a = self.atomic_scattering_coeffs[species][0]
         b = self.atomic_scattering_coeffs[species][1]
         c = self.atomic_scattering_coeffs[species][2]
-        f = c
-        for i in range(4):
-            f += a[i] * np.exp(-b[i] * (q_mag / (4 * np.pi))**2)
+        return c + np.sum(a * np.exp(-b * (q_mag / (4 * np.pi))**2))
 
-        return f
-
-    def plot(self):
+    def plot(self, **kwargs):
+        """ Wrapper function to plot the PXRD pattern. """
         from matador.plotting.pxrd_plotting import plot_pxrd
-        plot_pxrd(self)
+        plot_pxrd(self, **kwargs)
 
 
 class PXRDFactory(FingerprintFactory):
