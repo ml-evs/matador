f7fae5dc94405c1a455ddcdf7d494b2e619c91ef
Matthew Evans <me388@cam.ac.uk>
2020-05-19 15:07:04 +0100

    MAINT: updated method names for ComputeTask and removed FullRelaxer

--- matador/compute/__init__.py
+++ matador/compute/__init__.py
@@ -4,12 +4,12 @@
 """ The compute module contains three submodules, compute, batch and
 slurm.
 
-The compute submodule contains the FullRelaxer class for performing
+The compute submodule contains the :class:`ComputeTask` class for performing
 continually restarted geometry optimisation and SCF calculations in
 CASTEP, as well as the execution of arbitrary programs with mpirun.
 
-The batch submodule contains the BatchRun class for running several
-independent FullRelaxer instances on a folder of structures, without
+The batch submodule contains the :class:`BatchRun` class for running several
+independent :class:`ComputeTask` instances on a folder of structures, without
 clashes.
 
 The slurm submodule provides a wrapper to useful slurm commands, and to
@@ -18,10 +18,10 @@ writing slurm job submission files.
 """
 
 
-__all__ = ['ComputeTask', 'BatchRun', 'reset_job_folder', 'FullRelaxer']
+__all__ = ['ComputeTask', 'BatchRun', 'reset_job_folder']
 __author__ = 'Matthew Evans'
 __maintainer__ = 'Matthew Evans'
 
 
-from matador.compute.compute import FullRelaxer, ComputeTask
+from matador.compute.compute import ComputeTask
 from matador.compute.batch import BatchRun, reset_job_folder
