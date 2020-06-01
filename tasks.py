09671dc01dbc2dae4ebe5cc8f8dcaf07f32685cd
Matthew Evans <me388@cam.ac.uk>
2020-06-01 20:52:09 +0100

    BLD: added invoke task for pypi publishing

---
+++ tasks.py
@@ -0,0 +1,11 @@
+""" Some tasks for deployment to PyPI/TestPyPI. """
+
+
+from invoke import task
+
+
+@task
+def publish_to_pypi(c, test=False):
+    c.run("rm -rf build dist", warn=True)
+    c.run("python setup.py sdist bdist_wheel")
+    c.run("twine upload --verbose dist/*" + " --repository-url https://test.pypi.org/legacy/" if test else "")
