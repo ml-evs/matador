efc8c78e4cdd94a5ced073671275796014438402
Matthew Evans <me388@cam.ac.uk>
2020-06-03 16:16:40 +0100

    BLD: tweaked release task

--- tasks.py
+++ tasks.py
@@ -9,3 +9,4 @@ def publish_to_pypi(c, test=False):
     c.run("rm -rf build dist", warn=True)
     c.run("python setup.py sdist bdist_wheel")
     c.run("twine upload --verbose dist/*" + " --repository-url https://test.pypi.org/legacy/" if test else "")
+    c.run("rm -rf build dist", warn=True)
