8a6c43e32d9a89420863b3f13bae05f2929f7412
Matthew Evans <me388@cam.ac.uk>
2020-06-08 15:25:40 +0100

    BLD: updated changelog and bumped version for 0.9.4

--- matador/__init__.py
+++ matador/__init__.py
@@ -11,6 +11,6 @@ theory compute engines.
 __all__ = ['__version__']
 __author__ = 'Matthew Evans'
 __maintainer__ = 'Matthew Evans'
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 
 script_epilog = f"Written and maintained by Matthew Evans (me388@cam.ac.uk) 2016-2020, version {__version__}."
