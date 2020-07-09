65f189590b292c6b18411700cdacb7da7beb838c
Matthew Evans <me388@cam.ac.uk>
2020-07-09 15:59:32 +0100

    MAINT: remove leftover debug print

--- matador/hull/hull.py
+++ matador/hull/hull.py
@@ -204,7 +204,6 @@ class QueryConvexHull:
 
         if not self.args.get('no_plot'):
             if self.compute_voltages and self.voltage_data:
-                print('plotting voltage')
                 self.plot_voltage_curve(show=False)
             if self.compute_volumes and self.volume_data:
                 self.plot_volume_curve(show=False)
