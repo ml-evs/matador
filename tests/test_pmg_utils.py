e14acc2e9bf78ddf74aea0d5e97529b7e60cbc2b
Matthew Evans <me388@cam.ac.uk>
2020-07-23 13:23:28 +0100

    ENH: Added pmg2dict method and other keywords
    - As `as_model` to ase2dict and pmg2dict.
    - Added `.from_ase(...)` and `from_pmg(...)` methods to Crystal.

--- tests/test_pmg_utils.py
+++ tests/test_pmg_utils.py
@@ -43,3 +43,31 @@ class PMGUtilTest(unittest.TestCase):
             self.assertEqual(site.species, pmg_structure[ind].species_string)
 
         self.assertDictEqual(pmg_structure.info["matador"], doc._data)
+
+    def test_pmg2dict(self):
+        import ase.build
+        from pymatgen.io.ase import AseAtomsAdaptor
+        from matador.utils.pmg_utils import pmg2dict
+
+        # construct ASE atoms object manually
+        ase_atoms = ase.build.bulk("SiO", crystalstructure="rocksalt", a=12, cubic=True)
+
+        pmg_structure = AseAtomsAdaptor.get_structure(ase_atoms)
+        doc = pmg2dict(pmg_structure, as_model=True)
+
+        self.assertEqual(
+            doc["atom_types"], sorted(["Si", "O", "Si", "O", "Si", "O", "Si", "O"])
+        )
+
+        np.testing.assert_array_almost_equal(
+            pmg_structure.lattice.matrix, doc.lattice_cart, decimal=16
+        )
+        self.assertAlmostEqual(doc["cell_volume"], 12 ** 3)
+        self.assertEqual(len(doc["positions_frac"]), len(doc["atom_types"]))
+        self.assertEqual(doc["num_atoms"], 8)
+        self.assertEqual(doc["num_fu"], 4)
+        self.assertListEqual(doc["stoichiometry"][0], ["O", 1.0])
+        self.assertListEqual(doc["stoichiometry"][1], ["Si", 1.0])
+        self.assertListEqual(sorted(list(doc["elems"])), ["O", "Si"])
+        self.assertEqual(doc["space_group"], "Fm-3m")
+        self.assertEqual(pmg_structure.formula, "Si4 O4")
