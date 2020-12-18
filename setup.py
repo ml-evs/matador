23304112dba6e9945604492c88b2326e0720bc87
Matthew Evans <git@ml-evs.science>
2020-12-18 18:04:51 +0000

    Disable py39

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(name='matador-db',
           "Programming Language :: Python :: 3.6",
           "Programming Language :: Python :: 3.7",
           "Programming Language :: Python :: 3.8",
-          "Programming Language :: Python :: 3.9",
           "Topic :: Scientific/Engineering",
           "Topic :: Scientific/Engineering :: Chemistry",
           "Topic :: Scientific/Engineering :: Physics"
