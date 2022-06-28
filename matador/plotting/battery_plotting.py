6adfd644bfb80ef67caee6e9c2e5fd1e7e41df36
Matthew Evans <git@ml-evs.science>
2022-06-28 18:40:43 +0100

    Add option to plot guidelines for max cap/average voltage on electrode plots

--- matador/plotting/battery_plotting.py
+++ matador/plotting/battery_plotting.py
@@ -14,7 +14,7 @@ from matador.plotting.plotting import plotting_function, SAVE_EXTS
 from matador.battery import VoltageProfile
 
 
-__all__ = ['plot_voltage_curve', 'plot_volume_curve']
+__all__ = ["plot_voltage_curve", "plot_volume_curve"]
 
 
 @plotting_function
@@ -26,10 +26,12 @@ def plot_voltage_curve(
     savefig: Optional[str] = None,
     curve_labels: Optional[Union[str, List[str]]] = None,
     line_kwargs: Optional[Union[Dict, List[Dict]]] = None,
+    plot_average_voltage: bool = False,
+    plot_max_capacity: bool = False,
     expt: Optional[str] = None,
-    expt_label: Optional[str] = None
+    expt_label: Optional[str] = None,
 ):
-    """ Plot voltage curve calculated for phase diagram.
+    """Plot voltage curve calculated for phase diagram.
 
     Parameters:
         profiles (list/VoltageProfile): list of/single voltage profile(s).
@@ -61,30 +63,43 @@ def plot_voltage_curve(
 
     if curve_labels is not None and len(curve_labels) != len(profiles):
         raise RuntimeError(
-            "Wrong number of labels passed for number of profiles: {} vs {}"
-            .format(len(curve_labels), len(profiles))
+            "Wrong number of labels passed for number of profiles: {} vs {}".format(
+                len(curve_labels), len(profiles)
+            )
         )
 
     if line_kwargs is not None and len(line_kwargs) != len(profiles):
         raise RuntimeError(
-            "Wrong number of line kwargs passed for number of profiles: {} vs {}"
-            .format(len(line_kwargs), len(profiles))
+            "Wrong number of line kwargs passed for number of profiles: {} vs {}".format(
+                len(line_kwargs), len(profiles)
+            )
         )
 
     dft_label = None
     if expt is not None:
-        expt_data = np.loadtxt(expt, delimiter=',')
+        expt_data = np.loadtxt(expt, delimiter=",")
         if expt_label:
-            ax.plot(expt_data[:, 0], expt_data[:, 1], c='k', lw=2, ls='-', label=expt_label)
+            ax.plot(
+                expt_data[:, 0], expt_data[:, 1], c="k", lw=2, ls="-", label=expt_label
+            )
         else:
-            ax.plot(expt_data[:, 0], expt_data[:, 1], c='k', lw=2, ls='-', label='Experiment')
+            ax.plot(
+                expt_data[:, 0],
+                expt_data[:, 1],
+                c="k",
+                lw=2,
+                ls="-",
+                label="Experiment",
+            )
 
         if len(profiles) == 1:
-            dft_label = 'DFT (this work)'
+            dft_label = "DFT (this work)"
 
     for ind, profile in enumerate(profiles):
         if dft_label is None and curve_labels is None:
-            stoich_label = get_formula_from_stoich(profile.starting_stoichiometry, tex=True)
+            stoich_label = get_formula_from_stoich(
+                profile.starting_stoichiometry, tex=True
+            )
         else:
             stoich_label = None
 
@@ -92,11 +107,29 @@ def plot_voltage_curve(
         if curve_labels is not None and len(curve_labels) > ind:
             _label = curve_labels[ind]
 
-        _line_kwargs = {'c': list(plt.rcParams['axes.prop_cycle'].by_key()['color'])[ind+2]}
+        _line_kwargs = {
+            "c": list(plt.rcParams["axes.prop_cycle"].by_key()["color"])[ind + 2]
+        }
         if line_kwargs is not None:
             _line_kwargs.update(line_kwargs[ind])
 
-        _add_voltage_curve(profile.capacities, profile.voltages, ax, label=_label, **_line_kwargs)
+        _add_voltage_curve(
+            profile.capacities, profile.voltages, ax, label=_label, **_line_kwargs
+        )
+
+        if plot_average_voltage:
+            vline_kwargs = _line_kwargs
+            vline_kwargs["ls"] = "--"
+            vline_kwargs["alpha"] = 0.8
+            vline_kwargs["zorder"] = 0
+            ax.axhline(profile.average_voltage, **vline_kwargs)
+
+        if plot_max_capacity:
+            vline_kwargs = _line_kwargs
+            vline_kwargs["ls"] = "--"
+            vline_kwargs["alpha"] = 0.8
+            vline_kwargs["zorder"] = 0
+            ax.axvline(np.nanmax(profile.capacities), **vline_kwargs)
 
     if labels:
         if len(profiles) > 1:
@@ -110,18 +143,34 @@ def plot_voltage_curve(
                     _label = "{:.1f} ".format(phase[0])
                 _label += "{}".format(phase[1])
                 _labels.append(_label)
-            _label = '+'.join(_labels)
-            _position = (profiles[0].capacities[ind+1], profiles[0].voltages[ind+1] + max(profiles[0].voltages)*0.01)
-            ax.annotate(_label, xy=_position, textcoords="data", ha="center", zorder=9999)
+            _label = "+".join(_labels)
+            _position = (
+                profiles[0].capacities[ind + 1],
+                profiles[0].voltages[ind + 1] + max(profiles[0].voltages) * 0.01,
+            )
+            ax.annotate(
+                _label, xy=_position, textcoords="data", ha="center", zorder=9999
+            )
 
     if expt or len(profiles) > 1:
+        if plot_average_voltage and plot_max_capacity:
+            label = "Average voltages and maximum gravimetric capacities"
+        if plot_average_voltage and not plot_max_capacity:
+            label = "Average voltages"
+        if not plot_average_voltage and plot_max_capacity:
+            label = "Maximum gravimetric capacities"
+
+        if plot_average_voltage or plot_max_capacity:
+            ax.axvline(-1, c="k", ls="--", alpha=0.8, label=label)
+
         ax.legend()
 
-    ax.set_ylabel('Voltage (V) vs {ion}$^+/${ion}'.format(ion=profile.active_ion))
-    ax.set_xlabel('Gravimetric cap. (mAh/g)')
+    ax.set_ylabel("Voltage (V) vs {ion}$^+/${ion}".format(ion=profile.active_ion))
+    ax.set_xlabel("Gravimetric cap. (mAh/g)")
 
     _, end = ax.get_ylim()
     from matplotlib.ticker import MultipleLocator
+
     ax.yaxis.set_major_locator(MultipleLocator(0.2))
     ax.set_ylim(0, 1.1 * end)
     _, end = ax.get_xlim()
@@ -131,7 +180,7 @@ def plot_voltage_curve(
 
     if savefig:
         plt.savefig(savefig)
-        print('Wrote {}'.format(savefig))
+        print("Wrote {}".format(savefig))
 
     elif show:
         plt.show()
@@ -140,7 +189,7 @@ def plot_voltage_curve(
 
 
 def _add_voltage_curve(capacities, voltages, ax, label=None, **kwargs):
-    """ Add the voltage curves stored under hull['voltage_data'] to the plot.
+    """Add the voltage curves stored under hull['voltage_data'] to the plot.
 
     Parameters:
         capacities (list): list or numpy array of capacities.
@@ -155,23 +204,38 @@ def _add_voltage_curve(capacities, voltages, ax, label=None, **kwargs):
 
     """
     line_kwargs = {
-        'lw': 2,
-        'alpha': 1,
+        "lw": 2,
+        "alpha": 1,
     }
     line_kwargs.update(kwargs)
 
     for i in range(1, len(voltages) - 1):
         if i == 1 and label is not None:
-            ax.plot([capacities[i - 1], capacities[i]], [voltages[i], voltages[i]], label=label, **line_kwargs)
+            ax.plot(
+                [capacities[i - 1], capacities[i]],
+                [voltages[i], voltages[i]],
+                label=label,
+                **line_kwargs
+            )
         else:
-            ax.plot([capacities[i - 1], capacities[i]], [voltages[i], voltages[i]], **line_kwargs)
+            ax.plot(
+                [capacities[i - 1], capacities[i]],
+                [voltages[i], voltages[i]],
+                **line_kwargs
+            )
         if i != len(voltages) - 2:
-            ax.plot([capacities[i], capacities[i]], [voltages[i], voltages[i + 1]], **line_kwargs)
+            ax.plot(
+                [capacities[i], capacities[i]],
+                [voltages[i], voltages[i + 1]],
+                **line_kwargs
+            )
 
 
 @plotting_function
-def plot_volume_curve(hull, ax=None, show=True, legend=False, as_percentages=False, **kwargs):
-    """ Plot volume curve calculated for phase diagram.
+def plot_volume_curve(
+    hull, ax=None, show=True, legend=False, as_percentages=False, **kwargs
+):
+    """Plot volume curve calculated for phase diagram.
 
     Parameters:
         hull (matador.hull.QueryConvexHull): matador hull object.
@@ -195,45 +259,72 @@ def plot_volume_curve(hull, ax=None, show=True, legend=False, as_percentages=Fal
         ax = ax
 
     if as_percentages:
-        volume_key = 'volume_expansion_percentage'
+        volume_key = "volume_expansion_percentage"
     else:
-        volume_key = 'volume_ratio_with_bulk'
+        volume_key = "volume_ratio_with_bulk"
 
-    for j in range(len(hull.volume_data['electrode_volume'])):
-        c = list(plt.rcParams['axes.prop_cycle'].by_key()['color'])[j+2]
-        stable_hull_dist = hull.volume_data['hull_distances'][j]
-        if len(stable_hull_dist) != len(hull.volume_data['Q'][j]):
+    for j in range(len(hull.volume_data["electrode_volume"])):
+        c = list(plt.rcParams["axes.prop_cycle"].by_key()["color"])[j + 2]
+        stable_hull_dist = hull.volume_data["hull_distances"][j]
+        if len(stable_hull_dist) != len(hull.volume_data["Q"][j]):
             raise RuntimeError("This plot does not support --hull_cutoff.")
 
         ax.plot(
-            [q for ind, q in enumerate(hull.volume_data['Q'][j]) if stable_hull_dist[ind] == 0],
-            [v for ind, v in enumerate(hull.volume_data[volume_key][j]) if stable_hull_dist[ind] == 0],
-            marker='o', markeredgewidth=1.5, markeredgecolor='k', c=c, zorder=1000, lw=0,
+            [
+                q
+                for ind, q in enumerate(hull.volume_data["Q"][j])
+                if stable_hull_dist[ind] == 0
+            ],
+            [
+                v
+                for ind, v in enumerate(hull.volume_data[volume_key][j])
+                if stable_hull_dist[ind] == 0
+            ],
+            marker="o",
+            markeredgewidth=1.5,
+            markeredgecolor="k",
+            c=c,
+            zorder=1000,
+            lw=0,
         )
 
         ax.plot(
-            [q for ind, q in enumerate(hull.volume_data['Q'][j]) if stable_hull_dist[ind] == 0],
-            [v for ind, v in enumerate(hull.volume_data[volume_key][j]) if stable_hull_dist[ind] == 0],
-            lw=2, c=c,
-            label=("{}"
-                   .format(get_formula_from_stoich(hull.volume_data['endstoichs'][j], tex=True)))
+            [
+                q
+                for ind, q in enumerate(hull.volume_data["Q"][j])
+                if stable_hull_dist[ind] == 0
+            ],
+            [
+                v
+                for ind, v in enumerate(hull.volume_data[volume_key][j])
+                if stable_hull_dist[ind] == 0
+            ],
+            lw=2,
+            c=c,
+            label=(
+                "{}".format(
+                    get_formula_from_stoich(hull.volume_data["endstoichs"][j], tex=True)
+                )
+            ),
         )
 
     ax.set_xlabel("Gravimetric capacity (mAh/g)")
     if as_percentages:
-        ax.set_ylabel('Volume expansion (%)')
+        ax.set_ylabel("Volume expansion (%)")
     else:
-        ax.set_ylabel('Volume ratio with starting electrode')
+        ax.set_ylabel("Volume ratio with starting electrode")
 
-    if legend or len(hull.volume_data['Q']) > 1:
+    if legend or len(hull.volume_data["Q"]) > 1:
         ax.legend()
-    fname = '{}_volume'.format(''.join(hull.elements))
+    fname = "{}_volume".format("".join(hull.elements))
 
     if hull.savefig or any([kwargs.get(ext) for ext in SAVE_EXTS]):
         for ext in SAVE_EXTS:
             if hull.args.get(ext) or kwargs.get(ext):
-                plt.savefig('{}.{}'.format(fname, ext), transparent=True)
-                print('Wrote {}.{}'.format(fname, ext))
+                plt.savefig("{}.{}".format(fname, ext), transparent=True)
+                print("Wrote {}.{}".format(fname, ext))
 
     if show:
         plt.show()
+
+    return ax
