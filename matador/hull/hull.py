Correct volume scaling for elemental electrodes from ternary phase diagrams

matador/hull/hull.py
@@ -22,16 +22,21 @@ from matador.utils.chem_utils import (
     parse_element_string,
     get_padded_composition,
     get_num_intercalated,
+    get_subscripted_formula,
+    get_formation_energy,
 )
 from matador.utils.chem_utils import (
     get_generic_grav_capacity,
     get_formula_from_stoich,
     get_stoich_from_formula,
 )
-from matador.utils.chem_utils import get_formation_energy
-from matador.utils.cursor_utils import set_cursor_from_array, get_array_from_cursor
-from matador.utils.cursor_utils import display_results, recursive_get
-from matador.utils.cursor_utils import filter_cursor_by_chempots
+from matador.utils.cursor_utils import (
+    set_cursor_from_array,
+    get_array_from_cursor,
+    display_results,
+    recursive_get,
+    filter_cursor_by_chempots,
+)
 from matador.battery import Electrode, VoltageProfile
 from matador.hull.phase_diagram import PhaseDiagram
 
@@ -822,7 +827,8 @@ class QueryConvexHull:
             print(30 * "-")
             print(
                 "Assessing reaction {}, {}:".format(
-                    reaction_ind + 1, get_formula_from_stoich(endstoichs[reaction_ind])
+                    reaction_ind + 1,
+                    get_formula_from_stoich(endstoichs[reaction_ind], unicode_sub=True),
                 )
             )
             try:
@@ -873,8 +879,14 @@ class QueryConvexHull:
         ]
 
         print(f"{30*'='}\n{len(endstoichs)} starting point(s) found.")
-        for endstoich in endstoichs:
-            print(get_formula_from_stoich(endstoich), end=" ")
+        print(
+            ", ".join(
+                [
+                    get_formula_from_stoich(endstoich, unicode_sub=True)
+                    for endstoich in endstoichs
+                ]
+            )
+        )
         print(f"\n{30*'='}\n")
 
     def _calculate_binary_volume_curve(self):
@@ -1043,12 +1055,13 @@ class QueryConvexHull:
             comp[:, 2] = 1 - comp[:, 0] - comp[:, 1]
 
             # normalize the crossover composition to one formula unit of the starting electrode
-            if np.where(np.asarray(initial_comp[1:]) < EPS)[0].tolist():
-                norm = [1]
+            _norm = np.asarray(crossover[ind][1:]) / np.asarray(initial_comp[1:])
+            if np.isnan(_norm[0]):
+                norm = _norm[1]
             else:
-                norm = np.asarray(crossover[ind][1:]) / np.asarray(initial_comp[1:])
+                norm = _norm[0]
 
-            ratios_of_phases = np.linalg.solve(comp.T, crossover[ind] / norm[0])
+            ratios_of_phases = np.linalg.solve(comp.T, crossover[ind] / norm)
 
             # remove small numerical noise
             ratios_of_phases[np.where(ratios_of_phases < EPS)] = 0
@@ -1092,7 +1105,7 @@ class QueryConvexHull:
                                 str(round(chem[0], 3)) + " "
                                 if abs(chem[0] - 1) > EPS
                                 else "",
-                                chem[1],
+                                get_subscripted_formula(chem[1]),
                             )
                             for chem in region
                         ]

tests/test_battery.py
@@ -489,7 +489,11 @@ class VolumeTest(unittest.TestCase):
             )
 
         hull = QueryConvexHull(
-            cursor=cursor, elements=["K", "Sn", "P"], no_plot=True, voltage=True
+            cursor=cursor,
+            elements=["K", "Sn", "P"],
+            no_plot=True,
+            voltage=True,
+            include_elemental_electrodes=True,
         )
 
         np.testing.assert_array_almost_equal(
@@ -544,7 +548,11 @@ class VolumeTest(unittest.TestCase):
             )
 
         hull = QueryConvexHull(
-            cursor=cursor, elements=["K", "Sn", "P"], no_plot=True, voltage=True
+            cursor=cursor,
+            elements=["K", "Sn", "P"],
+            no_plot=True,
+            voltage=True,
+            include_elemental_electrodes=True,
         )
 
         np.testing.assert_array_almost_equal(
@@ -577,6 +585,87 @@ class VolumeTest(unittest.TestCase):
             )
 
         expected_reactions = [
+            [
+                [(1.0, "CoP3")],
+                [(1.0, "CoP2"), (1.0, "Li3P")],
+                [(1 / 6, "LiCo6P4", 3 - 4 / 6, "Li3P")],
+                [(3.0, "Li3P"), (1.0, "Co")],
+            ],
+            [
+                [(1.0, "CoP2")],
+                [(1 / 6, "LiCo6P4"), (2 - 4 / 6, "Li3P")],
+                [(2.0, "Li3P"), (1.0, "Co")],
+            ],
+            [
+                [(1.0, "CoP")],
+                [(1 / 4, "CoP2"), (3 / 6 * 1 / 4, "LiCo6P4")],
+                [(1 / 6, "LiCo6P4"), (1 - 4 / 6, "Li3P")],
+                [(1, "Li3P"), (1, "Co")],
+            ],
+            [
+                [(1.0, "Co2P")],
+                [(1 / 4, "LiCo6P4"), (2 - 6 / 4, "Co")],
+                [(1.0, "Li3P"), (2.0, "Co")],
+            ],
+        ]
+
+        for i in range(len(expected_reactions)):
+            for j, react in enumerate(expected_reactions[i]):
+                for k, elem in enumerate(react):
+                    self.assertEqual(hull.voltage_data[i].reactions[j][k][1], elem[1])
+                    self.assertAlmostEqual(
+                        hull.voltage_data[i].reactions[j][k][0],
+                        elem[0],
+                        msg="\nReaction ({}, {}, {}) did not match: {} vs {}".format(
+                            i, j, k, hull.voltage_data[i].reactions[j][k], elem
+                        ),
+                    )
+
+    def test_realistic_ternary_with_elemental(self):
+        cursor, f = res2dict(REAL_PATH + "/data/voltage_data/voltage-LiCoP-oqmd/*.res")
+        hull = QueryConvexHull(
+            cursor=cursor,
+            species="LiCoP",
+            voltage=True,
+            volume=True,
+            no_plot=True,
+            include_elemental_electrodes=True,
+        )
+
+        LiP_hull = QueryConvexHull(
+            cursor=cursor, species="LiP", voltage=True, volume=True, no_plot=True
+        )
+
+        np.testing.assert_array_almost_equal(
+            hull.volume_data["volume_ratio_with_bulk"][0],
+            LiP_hull.volume_data["volume_ratio_with_bulk"][0],
+        )
+
+        for key in hull.volume_data:
+            self.assertEqual(len(hull.volume_data[key]), 5)
+
+        expected = [
+            [1, 0.9847, 1.1515, 1.2739, 2.3432],
+            [1, 1.6635, 2.6445, 3.1302],
+            [1, 2.4353, 3.1457],
+            [1, 1.0983, 1.7114, 2.9253],
+            [1, 1.1339, 2.44096],
+        ]
+        for i in range(4):
+            np.testing.assert_array_almost_equal(
+                np.asarray(hull.volume_data["volume_ratio_with_bulk"][i]),
+                expected[i],
+                decimal=4,
+            )
+
+        expected_reactions = [
+            [
+                [(1.0, "P")],
+                [(1 / 7, "LiP7")],
+                [(1 / 7, "Li3P7")],
+                [(1.0, "LiP")],
+                [(1, "Li3P")],
+            ],
             [
                 [(1.0, "CoP3")],
                 [(1.0, "CoP2"), (1.0, "Li3P")],