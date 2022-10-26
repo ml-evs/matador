9766d5eb2ca787b50ca42a3866112a49718fd4da
Matthew Evans <7916000+ml-evs@users.noreply.github.com>
2022-10-26 14:27:54 +0100

    v0.10.0 release (#305)
    * Add deprecation warning for Python 3.7
    
    * Bump version number
    
    * Add v0.10.0 to changelog

--- matador/__init__.py
+++ matador/__init__.py
@@ -7,10 +7,21 @@ prediction, leveraging CASTEP and Quantum Epsresso as density-functional
 theory compute engines.
 """
 
+import sys
 
 __all__ = ["__version__"]
 __author__ = "Matthew Evans"
 __maintainer__ = "Matthew Evans"
-__version__ = "0.9.11"
+__version__ = "0.10.0"
 
-script_epilog = f"Written and maintained by Matthew Evans (me388@cam.ac.uk) 2016-2021, version {__version__}."
+script_epilog = f"Written and maintained by Matthew Evans (me388@cam.ac.uk) 2016-2022, version {__version__}."
+
+
+if sys.version_info.minor == 7:
+    import warnings
+
+    warnings.warn(
+        "v0.10 of the `matador` library will be the last to support Python 3.7. "
+        "Please upgrade to Python 3.8+ to use v0.11 and later versions of `matador`.",
+        DeprecationWarning,
+    )
