7b992333694596e7ef41a61db557fc15733e6b50
Angela Harper <harpaf13@gmail.com>
2022-11-24 14:30:51 +0000

    run pre-commit

--- matador/compute/compute.py
+++ matador/compute/compute.py
@@ -2056,7 +2056,7 @@ class ComputeTask:
         # so that it can be restarted from for a magres task
         mag_files = [
             f"{seed}.{num:04}.mag" for num in range(1, self.ncores * self.nnodes)
-        ]  
+        ]
         if os.path.isfile("%s.0001.mag" % (seed)):
             LOG.info("Copying .mag files into compute_dir")
             for _file in mag_files:
