445a25b041baf7beb4b184f52fd828cb442307c0
Matthew Evans <git@ml-evs.science>
2023-11-09 21:58:43 +0000

    Bump version number

--- matador/__init__.py
+++ matador/__init__.py
@@ -7,21 +7,9 @@ prediction, leveraging CASTEP and Quantum Epsresso as density-functional
 theory compute engines.
 """
 
-import sys
-
 __all__ = ["__version__"]
 __author__ = "Matthew Evans"
 __maintainer__ = "Matthew Evans"
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 script_epilog = f"Written and maintained by Matthew Evans (me388@cam.ac.uk) 2016-2022, version {__version__}."
-
-
-if sys.version_info.minor == 7:
-    import warnings
-
-    warnings.warn(
-        "v0.10 of the `matador` library will be the last to support Python 3.7. "
-        "Please upgrade to Python 3.8+ to use v0.11 and later versions of `matador`.",
-        DeprecationWarning,
-    )
